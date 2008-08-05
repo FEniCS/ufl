7bc047eeb82364c14c9d377bad14c1537a4fec85
Martin Sandve Alnæs <martinal@simula.no>
2008-08-05 16:26:56 +0200

    Added function to validate value shape of expression, only a sketch.

--- ufl/checks.py
+++ ufl/checks.py
@@ -1,7 +1,7 @@
 """Functions to check the validity of forms."""
 
 __authors__ = "Martin Sandve Alnes"
-__date__ = "2008-03-14 -- 2008-05-20"
+__date__ = "2008-03-14 -- 2008-08-05"
 
 from output import *
 from all import *
@@ -10,7 +10,125 @@ from analysis import *
 from predicates import *
 
 
+def value_shape(expression, dim):
+    """Evaluate the value shape of expression with given implicit dimension."""
+
+    # TODO: Implement this for all non-compound stuff. Or do we need this?
+    #       What we really want is perhaps to check the index dimensions, not the shapes...
+    #       That can be done in the index remapping function.
+    #elif isinstance(expression, Index):
+    #    tmp = expression.dim()
+    #    shape = (dim,) if tmp is None else (tmp,)
+
+    if isinstance(expression, Terminal):
+        if isinstance(expression, (BasisFunction, Function)):
+            shape = expression._element.value_shape()
+        elif isinstance(expression, (Number, Constant, MeshSize)):
+            shape = ()
+        elif isinstance(expression, FacetNormal):
+            shape = (dim,)
+        elif isinstance(expression, Identity):
+            shape = (dim, dim)
+        else:
+            ufl_error("Missing %s handler for computing value shape." % expression.__class__)
+    else:
+        ops_shapes = [value_shape(o, dim) for o in expression.operands()]
+
+        if isinstance(expression, Sum):
+            shape = ops_shapes[0]
+            ufl_assert(all(o == shape for o in ops_shapes), "Value shape mismatch.")
+
+        elif isinstance(expression, Variable):
+            shape = ops_shapes[0]
+        
+        elif isinstance(expression, Product):
+            ufl_error("Not implemented!")
+            
+        elif isinstance(expression, Division):
+            ufl_error("Not implemented!")
+            
+        elif isinstance(expression, Power):
+            ufl_error("Not implemented!")
+
+        elif isinstance(expression, (Mod, Abs, MathFunction)):
+            shape = ()
+            ufl_assert(ops_shapes[0] == (), "Non-scalar function argument.")
+        
+        elif isinstance(expression, Indexed):   
+            ufl_error("Not implemented!") # FIXME
+
+        elif isinstance(expression, PartialDerivative): 
+            ufl_error("Not implemented!") # FIXME
+            
+        elif isinstance(expression, Diff): 
+            ufl_error("Not implemented!") # FIXME
+            
+        elif isinstance(expression, Grad):
+            shape = (dim,) + ops_shapes[0]
+            
+        elif isinstance(expression, Div):
+            shape = ops_shapes[1:]
+            ufl_assert(ops_shapes[0] == dim, "Value shape mismatch in Div.")        
+
+        elif isinstance(expression, Curl): 
+            ufl_error("Not implemented!") # FIXME
+
+        elif isinstance(expression, Rot):  
+            ufl_error("Not implemented!") # FIXME
+
+        elif isinstance(expression, Outer): 
+            ufl_error("Not implemented!") # FIXME
+            
+        elif isinstance(expression, Inner): 
+            ufl_error("Not implemented!") # FIXME
+            
+        elif isinstance(expression, Dot):   
+            ufl_error("Not implemented!") # FIXME
+            
+        elif isinstance(expression, Cross): 
+            ufl_error("Not implemented!") # FIXME
+
+        elif isinstance(expression, Trace):     
+            ufl_error("Not implemented!") # FIXME
+            
+        elif isinstance(expression, Determinant): 
+            ufl_error("Not implemented!") # FIXME
+
+        elif isinstance(expression, Transpose): 
+            ufl_error("Not implemented!") # FIXME
+        
+        elif isinstance(expression, Inverse):     
+            ufl_error("Not implemented!") # FIXME
+        
+        elif isinstance(expression, Deviatoric):
+            ufl_error("Not implemented!") # FIXME
+        
+        elif isinstance(expression, Cofactor):
+            ufl_error("Not implemented!") # FIXME
+        
+        elif isinstance(expression, ListVector):
+            ufl_error("Not implemented!") # FIXME
+        
+        elif isinstance(expression, ListMatrix):
+            ufl_error("Not implemented!") # FIXME
+        
+        elif isinstance(expression, Tensor):
+            ufl_error("Not implemented!") # FIXME
+        
+        else:
+            ufl_error("Missing %s handler for computing value shape." % expression.__class__)
+
+    return shape
+
+
 def validate_form(a):
     """Performs all implemented validations on a form. Raises exception if something fails."""
-    ufl_warning("validate_form does nothing yet.")
-
+    
+    ufl_assert(isinstance(a, Form), "Expecting Form.")
+    
+    ufl_assert(is_multilinear(a), "Form is not multilinear.")
+    
+    for e in iter_expressions(a):
+        shape = value_shape(e)
+        ufl_assert(shape == (), "Got non-scalar integrand expression:\n%s" % e)
+    
