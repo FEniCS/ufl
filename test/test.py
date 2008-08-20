8d43a9c49522b2f511cf54f5ca26a40a85ced52e
Martin Sandve Alnæs <martinal@simula.no>
2008-08-20 15:42:30 +0200

    Changed test.py to run all tests in directory.

--- test/test.py
+++ test/test.py
@@ -4,13 +4,16 @@ __author__ = "Anders Logg (logg@simula.no)"
 __date__ = "2008-03-12 -- 2008-08-20"
 
 from os import system
+from glob import glob
 
 # disable log output
 import logging
 logging.basicConfig(level=logging.CRITICAL)
 
 # Tests to run
-tests = ["elements", "indices", "forms", "illegal", "algorithms"]
+#tests = ["elements", "indices", "forms", "illegal", "algorithms"]
+tests = [f.replace(".py", "") for f in glob("*.py")]
+tests.remove("test")
 
 # Run tests
 for test in tests:
