c019c1b4a4b19f57a45b33f88181de9a48f5b2ed
Martin Sandve Alnæs <martinal@simula.no>
2008-09-09 18:12:41 +0200

    Added utilities for working with accumulated tensor shapes in a flattened index space, for working with general mixed elements.

--- ufl/common.py
+++ ufl/common.py
@@ -59,8 +59,120 @@ class StackDict(dict):
             self[k] = v
         return k, v
 
+def strides(shape):
+    if len(shape) == 0:
+        return ()
+    stride = 1
+    result = [1]
+    for s in shape[-1:0:-1]:
+        stride *= s
+        result.append(stride)
+    return tuple(reversed(result))
 
-if __name__ == "__main__":
+def component_to_index(component, shape):
+    i = 0
+    for (c,s) in zip(component, strides(shape)):
+        i += c*s
+    return i
+
+def index_to_component(index, shape):
+    component = []
+    for s in strides(shape):
+        a = index // s
+        b = index % s
+        index = b
+        component.append(a)
+    return tuple(component)
+
+def test_component_indexing():
+    print
+    s = ()
+    print s, strides(s)
+    c = ()
+    q = component_to_index(c, s)
+    c2 = index_to_component(q, s)
+    print c, q, c2
+
+    print
+    s = (2,)
+    print s, strides(s)
+    for i in range(s[0]):
+        c = (i,)
+        q = component_to_index(c, s)
+        c2 = index_to_component(q, s)
+        print c, q, c2
+
+    print
+    s = (2,3)
+    print s, strides(s)
+    for i in range(s[0]):
+        for j in range(s[1]):
+            c = (i,j)
+            q = component_to_index(c, s)
+            c2 = index_to_component(q, s)
+            print c, q, c2
+
+    print
+    s = (2,3,4)
+    print s, strides(s)
+    for i in range(s[0]):
+        for j in range(s[1]):
+            for k in range(s[2]):
+                c = (i,j,k)
+                q = component_to_index(c, s)
+                c2 = index_to_component(q, s)
+                print c, q, c2
+
+    # Taylor-Hood example:
+
+    # pressure element is index 3:
+    c = (3,)
+    # get flat index:
+    i = component_to_index(c, (4,))
+    # remove offset:
+    i -= 3
+    # map back to component:
+    c = index_to_component(i, ())
+    print c
+
+    # vector element y-component is index 1:
+    c = (1,)
+    # get flat index:
+    i = component_to_index(c, (4,))
+    # remove offset:
+    i -= 0
+    # map back to component:
+    c = index_to_component(i, (3,))
+    print c
+
+    # Try a tensor/vector element:
+    mixed_shape = (6,)
+    ts = (2,2)
+    vs = (2,)
+    offset = 4
+
+    # vector element y-component is index offset+1:
+    c = (offset+1,)
+    # get flat index:
+    i = component_to_index(c, mixed_shape)
+    # remove offset:
+    i -= offset
+    # map back to vector component:
+    c = index_to_component(i, vs)
+    print c
+
+    for k in range(4):
+        # tensor element (1,1)-component is index 3:
+        c = (k,)
+        # get flat index:
+        i = component_to_index(c, mixed_shape)
+        # remove offset:
+        i -= 0
+        # map back to vector component:
+        c = index_to_component(i, ts)
+        print c
+
+def test_stackdict():
     d = StackDict(a=1)
     d.push("a", 2)
     d.push("a", 3)
@@ -70,3 +182,6 @@ if __name__ == "__main__":
     d.pop()
     print d
 
+if __name__ == "__main__":
+    test_component_indexing()
+    test_stackdict()
