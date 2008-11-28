eb731be477e97f760a94a599209fed75808db73c
Martin Sandve Alnæs <martinal@simula.no>
2008-11-28 11:33:59 +0100

    Added unused sketch of FormTransformation framework, considering using this to postphone computation of form transformations like the derivative until the compilation stage.

--- ufl/formoperators.py
+++ ufl/formoperators.py
@@ -1,13 +1,57 @@
 "Various high level ways to transform a complete Form into a new Form."
 
-
 __authors__ = "Martin Sandve Alnes"
-__date__ = "2008-03-14 -- 2008-10-30"
+__date__ = "2008-03-14 -- 2008-11-28"
 
-from ufl.algorithms import compute_form_derivative
-from ufl.algorithms import compute_form_adjoint, compute_form_action
-from ufl.algorithms import compute_form_lhs, compute_form_rhs
-#from ufl.algorithms import compute_dirichlet_functional
+from ufl.algorithms import compute_form_derivative, \
+                           compute_form_adjoint, compute_form_action, \
+                           compute_form_lhs, compute_form_rhs
+
+# TODO: If we need to apply transformations inside the compiler,
+# we must postphone the computations by using a framework like this.
+# This could be important for AD.
+
+class FormTransform(object):
+    def __init__(self):
+        pass
+
+    def __add__(self, other):
+        return FormTransformSum(self, other)
+
+class FormTransformSum(FormTransform):
+    def __init__(self, left, right):
+        FormTransform.__init__(self)
+        self._left = left
+        self._right = right
+
+class FormTransformDerivative(FormTransform):
+    def __init__(self, form, function, basisfunction):
+        FormTransform.__init__(self)
+        self._form = form
+        self._function = function
+        self._basisfunction = basisfunction
+
+class FormTransformAction(FormTransform):
+    def __init__(self, form, function):
+        FormTransform.__init__(self)
+        self._form = form
+        self._function = function
+
+class FormTransformAdjoint(FormTransform):
+    def __init__(self, form):
+        FormTransform.__init__(self)
+        self._form = form
+
+class FormTransformLhs(FormTransform):
+    def __init__(self, form):
+        FormTransform.__init__(self)
+        self._form = form
+
+class FormTransformRhs(FormTransform):
+    def __init__(self, form):
+        FormTransform.__init__(self)
+        self._form = form
+
 
 def rhs(form):
     """Given a combined bilinear and linear form,
@@ -18,11 +62,13 @@ def rhs(form):
     "-f*v*dx" as the rigth hand side should
     be when solving the equations?
     """
+    #return FormTransformRhs(form)
     return compute_form_rhs(form)
 
 def lhs(form):
     """Given a combined bilinear and linear form,
     extract the bilinear form part (left hand side)."""
+    #return FormTransformLhs(form)
     return compute_form_lhs(form)
 
 def action(form, function=None):
@@ -30,11 +76,13 @@ def action(form, function=None):
     with an additional function coefficient, representing
     the action of the form on the function. This can be
     used for matrix-free methods."""
+    #return FormTransformAction(form)
     return compute_form_action(form, function)
 
 def adjoint(form):
     """Given a combined bilinear form, compute the adjoint
     form by swapping the test and trial functions."""
+    #return FormTransformAdjoint(form)
     return compute_form_adjoint(form)
 
 def derivative(form, function, basisfunction=None):
@@ -45,8 +93,6 @@ def derivative(form, function, basisfunction=None):
     A tuple of Functions may be provided in place of
     a single Function, in which case the new BasisFunction
     argument is based on a MixedElement created from this tuple."""
+    #return FormTransformDerivative(form)
     return compute_form_derivative(form, function, basisfunction)
-
-#def dirichlet_functional(form):
-#    return compute_dirichlet_functional(form)
 
