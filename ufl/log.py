0a45431155b06ed761cb0828741113df4baea999
Anders Logg <logg@simula.no>
2009-01-13 22:57:03 +0100

    Move log functions to class ufl.log.Logger and create simple mechanism for creating global log functions for given log name.

--- ufl/log.py
+++ ufl/log.py
@@ -2,25 +2,18 @@
 output messages. These may be redirected by the user of UFL."""
 
 __author__ = "Martin Sandve Alnaes and Anders Logg"
-__date__ = "2005-02-04 -- 2009-01-12"
+__date__ = "2005-02-04 -- 2009-01-13"
 __copyright__ = "Copyright (C) 2005-2009 Anders Logg and Martin Sandve Alnaes"
 __license__  = "GNU GPL version 3 or any later version"
 
 import sys
 import logging
 
-__all__ = ["debug", "info", "warning", "error", "begin", "end",
-           "set_level", "set_indent", "add_indent",
-           "set_handler", "get_handler", "set_logger", "get_logger",
-           "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
+log_functions = ["debug", "info", "warning", "error", "begin", "end",
+                 "set_level", "set_indent", "add_indent",
+                 "set_handler", "get_handler", "set_logger", "get_logger"]
 
-# Set up logger and handler
-_log = logging.getLogger("UFL")
-_handler = logging.StreamHandler()
-_log.addHandler(_handler)
-
-# Set initial indentation level
-_indent_level = 0
+__all__ = log_functions + ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL", "Logger", "log_functions"]
 
 # Import default log levels
 from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
@@ -30,96 +23,98 @@ class UFLException(Exception):
     "Base class for UFL exceptions"
     pass
 
-#--- Functions for writing log messages ---
+# Logger class
+class Logger:
 
-def debug(*message):
-    "Write debug message."
-    _log.debug(_format(*message))
+    def __init__(self, name):
+        "Create logger instance."
 
-def info(*message):
-    "Write info message."
-    text = _format_raw(*message)
-    if len(text) >= 3 and text[-3:] == "...":
-        # FIXME: Using print directly here, don't know how to write without newline using logging
-        print _format(text),
-        sys.stdout.flush()
-    else:
-        _log.info(_format(*message))
+        # Set up logger and handler
+        self._log = logging.getLogger(name)        
+        self._handler = logging.StreamHandler()
+        self._log.addHandler(self._handler)
 
-def warning(*message):
-    "Write warning message."
-    _log.warning(_format(*message))
+        # Set initial indentation level
+        self._indent_level = 0
 
-def error(*message):
-    "Write error message and raise an exception."
-    _log.error(*message)
-    raise UFLException(_format_raw(*message))
+    def debug(self, *message):
+        "Write debug message."
+        self._log.debug(self._format(*message))
 
-def begin(*message):
-    "Begin task: write message and increase indentation level."
-    info(*message)
-    info("-"*len(_format_raw(*message)))
-    add_indent()
+    def info(self, *message):
+        "Write info message."
+        text = self._format_raw(*message)
+        if len(text) >= 3 and text[-3:] == "...":
+            # FIXME: Using print directly here, don't know how to write without newline using logging
+            print self._format(text),
+            sys.stdout.flush()
+        else:
+            self._log.info(self._format(*message))
 
-def end():
-    "End task: write a newline and decrease indentation level."
-    info("")
-    add_indent(-1)
+    def warning(self, *message):
+        "Write warning message."
+        self._log.warning(self._format(*message))
 
+    def error(self, *message):
+        "Write error message and raise an exception."
+        self._log.error(*message)
+        raise UFLException(self._format_raw(*message))
+
+    def begin(self, *message):
+        "Begin task: write message and increase indentation level."
+        self.info(*message)
+        self.info("-"*len(self._format_raw(*message)))
+        self.add_indent()
+
+    def end(self):
+        "End task: write a newline and decrease indentation level."
+        self.info("")
+        self.add_indent(-1)
+
+    def set_level(self, level):
+        "Set log level."
+        self._log.setLevel(level)
+        
+    def set_indent(self, level):
+        "Set indentation level."
+        self._indent_level = level
+
+    def add_indent(self, increment=1):
+        "Add to indentation level."
+        self._indent_level += increment
+
+    def get_handler(self):
+        "Get stream handler for logging."
+        return self._handler
+
+    def set_handler(self, handler):
+        "Set stream handler for logging."
+        self._log.removeHandler(_handler)    
+        self._log.addHandler(handler)
+        self._handler = handler
+
+    def get_logger(self):
+        "Return message logger."
+        return self._log
+
+    def _format(self, *message):
+        "Format message including indentation."
+        indent = 2*self._indent_level*" "
+        return indent + message[0] % message[1:]
+
+    def _format_raw(self, *message):
+        "Format message without indentation."
+        return message[0] % message[1:]
+
+#--- Set up global log functions ---
+
+ufl_logger = Logger("UFL")
+
+for foo in log_functions:
+    exec("%s = lambda *message : ufl_logger.%s(*message)" % (foo, foo))
+
+#--- Set up assertion ---
 def ufl_assert(condition, *message):
     "Assert that condition is true and otherwise issue an error with given message."
     if not condition:
         error(*message)
-
-#--- Functions for controlling output ---
-
-def set_level(level):
-    "Set log level."
-    _log.setLevel(level)
-
-def set_indent(level):
-    "Set indentation level."
-    global _indent_level
-    _indent_level = level
-
-def add_indent(increment=1):
-    "Add to indentation level."
-    global _indent_level
-    _indent_level += increment
-
-#--- Functions for setting up logger and handler ---
-
-def get_handler():
-    "Get stream handler for logging."
-    return _handler
-
-def set_handler(handler):
-    "Set stream handler for logging."
-    global _handler
-    _log.removeHandler(_handler)    
-    _log.addHandler(_handler)
-    _handler = handler
-
-def get_logger():
-    "Return message logger."
-    return _log
-
-def set_logger(logger):
-    "Set message logger."
-    global _logger
-    print "DO NOT USE THIS! WILL BE REMOVED!"
-    if isinstance(logger, logging.Logger):
-        _logger = logger
-    else:
-        _logger = logging.getLogger(logger)
-
-#--- Special functions used by logger ---
-
-def _format(*message):
-    "Format message including indentation."
-    indent = 2*_indent_level*" "
-    return indent + message[0] % message[1:]
-
-def _format_raw(*message):
-    "Format message without indentation."
-    return message[0] % message[1:]
