316f33eb317ee7bd1dd35778990037a2e253829c
Martin Sandve Alnæs <martinal@simula.no>
2009-02-15 16:28:36 +0100

    stuff

--- sandbox/printclasstree.py
+++ sandbox/printclasstree.py
@@ -9,7 +9,9 @@ for c in all_ufl_classes:
     subgraphs[c.mro()[1].__name__].append(c.__name__)
 
 # Recursive graph formatting
-def format_children(parent, level, skipparent=False):
+def format_children(parent, level, skipparent=True, maxlevels=None):
+    if maxlevels is not None and maxlevels < 0:
+        return ""
     children = subgraphs[parent]
     t = "  "*level
     begin = t + "subgraph {\n"
@@ -18,14 +20,22 @@ def format_children(parent, level, skipparent=False):
     for child in children:
         if child in subgraphs:
             g += begin
-            g += format_children(child, level+1)
+            g += format_children(child, level+1, skipparent=False, maxlevels=None if maxlevels is None else maxlevels-1)
             g += end
         if not skipparent:
             g += t + "%s -> %s;\n" % (child, parent)
     return g
 
 # Render graph body!
-body = format_children("object", 1, True)
+# All:
+body = format_children("object", 1)
+body = format_children("Expr", 1, False)
+# Root + some:
+body = format_children("object", 1, maxlevels=2)
+# Terminals:
+body = format_children("Terminal", 1, False)
+# Operators:
+body = format_children("Operator", 1, False)
 
 # Set global formatting options
 format = """
