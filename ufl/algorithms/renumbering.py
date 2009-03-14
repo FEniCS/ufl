599e863a206cb9d9a17bad4815ff0c810703b527
Martin Sandve Alnæs <martinal@simula.no>
2009-03-14 14:25:35 +0100

    Implemented renumbering of variable labels in renumber_indices.

--- ufl/algorithms/renumbering.py
+++ ufl/algorithms/renumbering.py
@@ -1,9 +1,10 @@
 __authors__ = "Martin Sandve Alnes and Anders Logg"
-__date__ = "2009-02-22 -- 2009-02-24"
+__date__ = "2009-02-22 -- 2009-03-14"
 
 from ufl.common import Counted
 from ufl.indexing import Index, FixedIndex, MultiIndex
 from ufl.basisfunction import BasisFunction
+from ufl.variable import Label, Variable
 from ufl.algorithms.transformations import ReuseTransformer, apply_transformer
 
 class IndexRenumberingTransformer(ReuseTransformer):
@@ -11,6 +12,18 @@ class IndexRenumberingTransformer(ReuseTransformer):
     def __init__(self):#, functions, basis_functions):
         ReuseTransformer.__init__(self)
         self._index_map = {}
+        self._variable_map = {}
+
+    def variable(self, o):
+        e, l = o.operands()
+        c = l.count()
+        v = self._variable_map.get(c)
+        if v is None:
+            e = self.visit(e)
+            l = Label(len(self._variable_map))
+            v = Variable(e, l)
+            self._variable_map[c] = v
+        return v
 
     def index_annotated(self, o):
         free_indices = tuple(map(self.index, o.free_indices()))
