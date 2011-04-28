e91c8fef74030240771f7eac4a96c95587f993f8
Martin Alnæs <martinal@simula.no>
2011-04-28 13:06:20 +0200

    Add objects for cell1D,2D,3D, and import those as well as zero in the ufl namespace.

--- ufl/objects.py
+++ ufl/objects.py
@@ -3,7 +3,7 @@
 __authors__ = "Martin Sandve Alnes"
 __copyright__ = "Copyright (C) 2008-2011 Martin Sandve Alnes"
 __license__  = "GNU LGPL version 3 or any later version"
-__date__ = "2008-03-14 -- 2009-06-19"
+__date__ = "2008-03-14 -- 2011-04-28"
 
 # Modified by Anders Logg, 2008
 # Modified by Kristian Oelgaard, 2009
@@ -30,6 +30,9 @@ R2 = Space(2)
 R3 = Space(3)
 
 # Cell types
+cell1D        = Cell("cell1D", 1, R1)
+cell2D        = Cell("cell2D", 1, R2)
+cell3D        = Cell("cell3D", 1, R3)
 vertex        = Cell("vertex", 1, R0)
 interval      = Cell("interval", 1, R1)
 triangle      = Cell("triangle", 1, R2)
@@ -38,4 +41,5 @@ quadrilateral = Cell("quadrilateral", 1, R2)
 hexahedron    = Cell("hexahedron", 1, R3)
 
 # TODO: Facet is just a dummy declaration, what can we do?
+# TODO: Remove this and just require the string "facet" where needed?
 facet         = Cell("facet", 1, R0)
