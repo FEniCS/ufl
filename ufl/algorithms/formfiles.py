40420fc2687fd6b8c5b0b480036f5e250e6752f6
Martin Alnes <martinal@simula.no>
2011-05-07 14:56:44 +0200

    Some improvements to load_ufl_file.
    Cleaned up duplicated tuple->form mapping and fixed potential bugs:
    mydict[id(as_form(value))] = value # used the id of a temporary object, makes no sense
    
    Renamed functions -> coefficients.
    Did this in a backwards compatible way to avoid breaking anything.
    
    Added object_by_name dict to FileData to allow getting forms,
    coefficients, and elements by their name in the .ufl file.
    
    Added reserved_objects dict to FileData to handle
    the 'unknown' coefficient in a more generic way.
    Did this in a backwards compatible way to avoid breaking anything.
    
    Added expressions list to FileData to allow explicit
    exporting of expressions from a .ufl file, e.g.
      forms = [a, L]
      expressions = [myexpr1, myexpr2]
    this is intended for some future ideas.
    One idea is as a debugging tool, another is to generate
    dolfin::Expression subclasses from ufl.Expr objects.

--- ufl/algorithms/formfiles.py
+++ ufl/algorithms/formfiles.py
@@ -29,12 +29,17 @@ from ufl.algorithms.tuplenotation import as_form
 class FileData(object):
     def __init__(self):
         self.elements     = []
-        self.functions    = []
+        self.functions    = [] # FIXME: Remove this member once checked that FFC does not use this so nothing is broken
+        self.coefficients = []
+        self.expressions  = []
         self.forms        = []
         self.object_names = {}
+        self.object_by_name = {}
+        self.reserved_objects = {}
 
     def __nonzero__(self):
-        return bool(self.elements or self.functions or self.forms)
+        return bool(self.elements or self.coefficients or self.forms or self.expressions or\
+                    self.object_names or self.object_by_name or self.reserved_objects)
 
 infostring = """An exception occured during evaluation of form file.
 To help you find the location of the error, a temporary script
@@ -42,7 +47,7 @@ To help you find the location of the error, a temporary script
 has been created and will now be executed with debug output enabled:"""
 
 def read_ufl_file(filename):
-    "Load a .ufl file with elements, functions and forms."
+    "Load a .ufl file with elements, coefficients and forms."
     if not os.path.exists(filename) and filename[-4:] != ".ufl":
         filename = filename + ".ufl"
     if not os.path.exists(filename):
@@ -69,14 +74,8 @@ def read_ufl_file(filename):
 
     return fcode
 
-def load_ufl_file(filename):
-    "Load a .ufl file with elements, functions and forms."
-    if not os.path.exists(filename) and filename[-4:] != ".ufl":
-        filename = filename + ".ufl"
-    if not os.path.exists(filename):
-        error("File '%s' doesn't exist." % filename)
-
-    # Read code
+def execute_ufl_code(filename):
+    # Read code from file
     fcode = read_ufl_file(filename)
 
     # Execute code
@@ -95,73 +94,88 @@ def load_ufl_file(filename):
         warning(infostring % pyname)
         m = __import__(basename)
         error("An error occured, aborting load_forms.")
+    return namespace
+
+def load_ufl_file(filename):
+    "Load a .ufl file with elements, coefficients and forms."
+    namespace = execute_ufl_code(filename)
 
     # Object to hold all returned data
     ufd = FileData()
 
-    # Extract Form objects, and Coefficient objects to get their names
-    all_forms = []
-
-    # Is id() necessary? Yes! Most likely. Otherwise some __hash__ thing
-    # might kick in and give us the same hash for different objects
-
-    # Extract object names
+    # Extract object names for Form, Coefficient and FiniteElementBase objects
+    # The use of id(obj) as key in object_names is necessary
+    # because we need to distinguish between instances,
+    # and not just between objects with different values.
     for name, value in namespace.iteritems():
-        if isinstance(value, (Form, tuple)):
-            # Convert tuple notation to form
-            all_forms.append(as_form(value))
-            ufd.object_names[id(as_form(value))] = name
-        elif isinstance(value, (Coefficient, Argument)):
-            if name is "unknown":
-                # TODO: No point in placing this in the object_names dict, make a new variable instead?
-                #ufd.unknown_coefficient = value
-                ufd.object_names[name] = value
-            ufd.object_names[id(value)] = name
-        elif isinstance(value, FiniteElementBase):
-            ufd.object_names[id(value)] = name
+        # Map tuples to forms if possible
+        if isinstance(value, tuple):
+            value = as_form(value)
+            if not isinstance(value, Form):
+                continue
 
-    # Get list of forms
+        # Get objects with reserved names
+        reserved_names = ("unknown",) # Currently only one reserved name
+        if name in reserved_names:
+            ufd.reserved_objects[name] = value
+            ufd.object_names[name] = value # FIXME: Remove after FFC is updated to use reserved_objects
+
+        # Store instance <-> name mappings for important objects
+        if isinstance(value, (FiniteElementBase, Coefficient, Argument, Form)):
+            ufd.object_names[id(value)] = name
+            ufd.object_by_name[name] = value
+
+    # Get list of exported forms
     forms = namespace.get("forms")
     if forms is None:
-        forms = [namespace.get(name) for name in ("a", "L", "M")]
-        # Convert tuple notation to form
-        forms = [as_form(a) for a in forms if a is not None]
-    # Convert tuple type forms to Form instances
-    ufl_assert(isinstance(forms, (list, tuple)),
-        "Expecting 'forms' to be a list or tuple, not '%s'." % type(forms))
-    ufl_assert(all(isinstance(a, Form) for a in forms),
-        "Expecting 'forms' to be a list of Form instances.")
+        # Get forms from object_by_name, which has already mapped tuple->Form where needed
+        forms = [ufd.object_by_name.get(name) for name in ("a", "L", "M")]
+        forms = [f for f in forms if f is not None]
     ufd.forms = forms
 
-    # Get list of elements
+    # Validate types
+    ufl_assert(isinstance(ufd.forms, (list, tuple)),
+        "Expecting 'forms' to be a list or tuple, not '%s'." % type(ufd.forms))
+    ufl_assert(all(isinstance(a, Form) for a in ufd.forms),
+        "Expecting 'forms' to be a list of Form instances.")
+
+    # Get list of exported elements
     elements = namespace.get("elements")
     if elements is None:
-        elements = [namespace.get(name) for name in ("element",)]
-        elements = [e for e in elements if not e is None]
-    ufl_assert(isinstance(elements,  (list, tuple)),
-        "Expecting 'elements' to be a list or tuple, not '%s'." % type(elements))
-    ufl_assert(all(isinstance(e, FiniteElementBase) for e in elements),
-        "Expecting 'elements' to be a list of FiniteElementBase instances.")
+        elements = [ufd.object_by_name.get(name) for name in ("element",)]
+        elements = [e for e in elements if e is not None]
     ufd.elements = elements
 
-    # Relevant functions will be extracted from forms
+    # Validate types
+    ufl_assert(isinstance(ufd.elements,  (list, tuple)),
+        "Expecting 'elements' to be a list or tuple, not '%s'." % type(ufd.elements))
+    ufl_assert(all(isinstance(e, FiniteElementBase) for e in ufd.elements),
+        "Expecting 'elements' to be a list of FiniteElementBase instances.")
+
+    # Get list of exported coefficients
+    # Temporarily let 'coefficients' override 'functions', but allow 'functions' for compatibility
     functions = namespace.get("functions", [])
     if functions:
-        warning("List of functions not implemented.")
-    ufl_assert(isinstance(functions, (list, tuple)),
-        "Expecting 'functions' to be a list or tuple, not '%s'." % type(functions))
-    ufl_assert(all(isinstance(e, Coefficient) for e in functions),
-        "Expecting 'functions' to be a list of Coefficient instances.")
-    ufd.functions = functions
+        warning("Deprecation warning: Rename 'functions' to 'coefficients' to export coefficients.")
+    ufd.coefficients = namespace.get("coefficients", functions)
+    ufd.functions = ufd.coefficients
+    # FIXME: Remove the above for FEniCS 1.0, this is enough:
+    #ufd.coefficients = namespace.get("coefficients", [])
 
-    # FIXME: Need to call validate_form elsewhere!
+    # Validate types
+    ufl_assert(isinstance(ufd.coefficients, (list, tuple)),
+        "Expecting 'coefficients' to be a list or tuple, not '%s'." % type(ufd.coefficients))
+    ufl_assert(all(isinstance(e, Coefficient) for e in ufd.coefficients),
+        "Expecting 'coefficients' to be a list of Coefficient instances.")
 
-    # Analyse validity of forms
-    #for form in forms:
-    #    validate_form(form)
-        #errors = validate_form(form) # TODO: validate_form raises exception, it doesn't return errors
-        #if errors:
-        #    error("Found errors in form '%s':\n%s" % (form_names[id(form)], errors))
+    # Get list of exported expressions
+    ufd.expressions = namespace.get("expressions", [])
+
+    # Validate types
+    ufl_assert(isinstance(ufd.expressions, (list, tuple)),
+        "Expecting 'expressions' to be a list or tuple, not '%s'." % type(ufd.expressions))
+    ufl_assert(all(isinstance(e, Expr) for e in ufd.expressions),
+        "Expecting 'expressions' to be a list of Expr instances.")
 
     # Return file data
     return ufd
