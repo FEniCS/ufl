f2361a081177c2637a23b81484f20468fa9e30e3
Martin Sandve Alnæs <martinal@simula.no>
2011-06-01 15:36:13 +0200

    Add extract_domaindata function.

--- ufl/algorithms/analysis.py
+++ ufl/algorithms/analysis.py
@@ -277,6 +277,22 @@ def unique_tuple(objects):
             unique_objects.append(object)
     return tuple(unique_objects)
 
+def extract_domaindata(form):
+    "Extract the domaindata attached to integrals of each domain type in form."
+    domaindata = {}
+    for integral in form.integrals():
+        domain_type = integral.measure().domain_type()
+        data = integral.measure().domaindata()
+        # Check that there is only one domaindata object for each integral type
+        existing_data = domaindata.get(domain_type)
+        if existing_data is None:
+            domaindata[domain_type] = data
+        else:
+            # Not assuming anything about domaindata type, not even an equals operator!
+            ufl_assert(existing_data is data,
+                       "Found two domain data objects for same intergral type.")
+    return tuple(map(domaindata.get, _domain_types))
+
 def extract_num_sub_domains(form):
     "Extract the upper limit of sub domain ids for each domain type."
     num_domains = {}
