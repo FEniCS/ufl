d4000c1843352b477826367ce13f180eb0f804d2
Johannes Ring <johannr@simula.no>
2011-06-02 21:47:11 +0200

    Remove extra spaces in license header.

--- ufl/__init__.py
+++ ufl/__init__.py
@@ -128,11 +128,11 @@ A very brief overview of the language contents follows:
 #
 # UFL is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with UFL.  If not, see <http://www.gnu.org/licenses/>.
+# along with UFL. If not, see <http://www.gnu.org/licenses/>.
 #
 # Modified by Kristian Oelgaard, 2009
 # Modified by Anders Logg, 2009.
--- ufl/operators.py
+++ ufl/operators.py
@@ -14,11 +14,11 @@ objects."""
 #
 # UFL is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with UFL.  If not, see <http://www.gnu.org/licenses/>.
+# along with UFL. If not, see <http://www.gnu.org/licenses/>.
 #
 # First added:  2008-04-09
 # Last changed: 2010-12-04
