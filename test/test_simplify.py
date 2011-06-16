d6d07628264b589fbe844d4b9fa1118df66630dc
Martin Sandve Alnæs <martinal@simula.no>
2011-06-16 10:03:24 +0200

    Update changelog and added a small test.

--- test/test_simplify.py
+++ test/test_simplify.py
@@ -2,7 +2,7 @@
 
 from ufltestcase import UflTestCase, main
 from ufl.classes import Sum, Product
-
+import math
 from ufl import *
 
 class ElementsTestCase(UflTestCase):
@@ -31,7 +31,7 @@ class ElementsTestCase(UflTestCase):
         a = Product(f,f,f,f)
         b = f**4
         self.assertEqual(a, b)
-        
+ 
     def test_sums(self):
         element = FiniteElement("CG", "triangle", 1)
         f = Coefficient(element)
@@ -68,6 +68,17 @@ class ElementsTestCase(UflTestCase):
         a = f + f + g # collapsed
         b = g + (f + f) # collapsed
         self.assertEqual(a, b)
+
+    def test_mathfunctions(self):
+        for i in (0.1, 0.3, 0.9):
+            self.assertEqual(math.sin(i), sin(i))
+            self.assertEqual(math.cos(i), cos(i))
+            self.assertEqual(math.tan(i), tan(i))
+            self.assertEqual(math.asin(i), asin(i))
+            self.assertEqual(math.acos(i), acos(i))
+            self.assertEqual(math.atan(i), atan(i))
+            self.assertEqual(math.exp(i), exp(i))
+            self.assertEqual(math.log(i), ln(i))
 
 if __name__ == "__main__":
     main()
