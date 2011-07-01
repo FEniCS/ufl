b175bb191bd21171aa7473729b6cfa3025a23fbc
Kristian B. Ølgaard <k.b.oelgaard@gmail.com>
2011-07-01 13:43:43 +0200

    Fix integer division bug in dev() operator. Related to Bug #804160 in FFC.

--- ufl/algorithms/transformations.py
+++ ufl/algorithms/transformations.py
@@ -410,9 +410,9 @@ class CompoundExpander(ReuseTransformer):
     def deviatoric(self, o, A):
         sh = self._square_matrix_shape(A)
         if sh[0] == 2:
-            return as_matrix([[-1/2*A[1,1]+1/2*A[0,0],A[0,1]],[A[1,0],1/2*A[1,1]-1/2*A[0,0]]])
+            return as_matrix([[-1./2*A[1,1]+1./2*A[0,0],A[0,1]],[A[1,0],1./2*A[1,1]-1./2*A[0,0]]])
         elif sh[0] == 3:
-            return as_matrix([[-1/3*A[1,1]-1/3*A[2,2]+2/3*A[0,0],A[0,1],A[0,2]],[A[1,0],2/3*A[1,1]-1/3*A[2,2]-1/3*A[0,0],A[1,2]],[A[2,0],A[2,1],-1/3*A[1,1]+2/3*A[2,2]-1/3*A[0,0]]])
+            return as_matrix([[-1./3*A[1,1]-1./3*A[2,2]+2./3*A[0,0],A[0,1],A[0,2]],[A[1,0],2./3*A[1,1]-1./3*A[2,2]-1./3*A[0,0],A[1,2]],[A[2,0],A[2,1],-1./3*A[1,1]+2./3*A[2,2]-1./3*A[0,0]]])
         error("dev(A) not implemented for dimension %s." % sh[0])
 
     def skew(self, o, A):
