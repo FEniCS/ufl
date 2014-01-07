9cdd8ba1273f66c124172b17c3037f31ab49e15e
Johannes Ring <johannr@simula.no>
2014-01-07 14:32:39 +0100

    Update version number to 1.3.0+.

--- ufl/__init__.py
+++ ufl/__init__.py
@@ -165,7 +165,7 @@ A very brief overview of the language contents follows:
 #
 # Last changed: 2014-01-07
 
-__version__ = "1.3.0"
+__version__ = "1.3.0+"
 
 ########## README
 # Imports here should be what the user sees when doing "from ufl import *",
