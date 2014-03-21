e6ed4f5b7abb413b61e8a0dd94d8958117bfacad
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-03-21 14:48:58 +0000

    Indexed shouldn't override __getnewargs__

--- ufl/indexed.py
+++ ufl/indexed.py
@@ -100,6 +100,3 @@ class Indexed(WrapperType):
 
     def __getitem__(self, key):
         error("Attempting to index with %r, but object is already indexed: %r" % (key, self))
-
-    def __getnewargs__(self):
-        return ()
