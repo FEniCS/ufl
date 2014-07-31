1e934cc8764e545cb2ecb05681687afcbac0d0a7
Martin Sandve Alnæs <martinal@simula.no>
2014-07-31 15:45:25 +0200

    New non-recursive implementation of expr_equals, 25% faster in a complex test case.

--- ufl/exprequals.py
+++ ufl/exprequals.py
@@ -1,147 +1,148 @@
 
+from collections import defaultdict
+from six.moves import zip
+
+from ufl.expr import Expr
 from ufl.log import error
 from ufl.operatorbase import Operator
 from ufl.terminal import Terminal
 from ufl.common import pre_traversal
-from six.moves import zip
 
-def _expr_equals0(a, b):
-    # Cutoff for different type
-    if type(a) != type(b):
-        return False
 
-    # Cutoff for same object
-    if a is b:
-        return True
-
-    # Iterate over pairs of potentially matching subexpressions
-    input = [(a, b)]
-    while input:
-        a, b = input.pop()
-
-        # Cutoff for different type
-        if type(a) != type(b):
-            return False
-
-        # Get operands
-        aops = a.ufl_operands
-        bops = b.ufl_operands
-        if aops:
-            if len(aops) != len(bops):
-                return False
-            # Add children for checking
-            input.extend(zip(aops, bops))
-        else:
-            # Compare terminals
-            if not a == b:
-                return False
-
-    # Everything checked out fine, expressions must be equal
-    return True
-
-def _expr_equals1(a, b):
-    # Cutoff for different type
-    if type(a) != type(b):
-        return False
-    # Cutoff for same object
-    if a is b:
-        return True
-    # Compare entire expression structure
-    for x, y in zip(pre_traversal(a), pre_traversal(b)):
-        if type(x) != type(y):
-            return False
-        #if isinstance(Terminal, x) and not x == y:
-        if x.ufl_operands == () and not x == y:
-            return False
-    # Equal terminals and types, a and b must be equal
-    return True
-
-def _expr_equals2(a, b):
-    # Cutoff for different type
-    if type(a) != type(b):
-        return False
-    # Cutoff for same object
-    if a is b:
-        return True
-    from ufl.algorithms.traversal import traverse_terminals, traverse_operands
-    # Check for equal terminals
-    for x, y in zip(traverse_terminals(a), traverse_terminals(b)):
-        if x != y:
-            return False
-    # Check for matching operator types
-    for x, y in zip(traverse_operands(a), traverse_operands(b)):
-        if type(x) != type(y):
-            return False
-    # Equal terminals and operands, a and b must be equal
-    return True
-
-equalsrecursed = {}
-equalscalls = {}
-collisions = {}
+hash_total = defaultdict(int)
+hash_collisions = defaultdict(int)
+hash_equals = defaultdict(int)
+hash_notequals = defaultdict(int)
 def print_collisions():
-    print("Collision statistics:")
-    keys = sorted(equalscalls.keys(), key=lambda x: collisions.get(x, 0))
-    for k in keys:
-        co = collisions.get(k, 0)
-        ca = equalscalls[k]
-        print((k, co, ca, int(100.0*co/ca)))
-    print("Recursion statistics:")
-    keys = sorted(keys, key=lambda x: equalsrecursed.get(x, 0))
-    for k in keys:
-        r = equalsrecursed.get(k, 0)
-        ca = equalscalls[k]
-        print((k, r, ca, int(100.0*r/ca)))
 
-def _expr_equals3(self, other): # Much faster than the more complex algorithms above!
+    keys = sorted(hash_total.keys(), key=lambda x: (hash_collisions[x], x))
+
+    print("Collision statistics ({0} keys):".format(len(keys)))
+    print("[key: equals; notequals; collisions]")
+    n = max(len(str(k)) for k in keys)
+    fmt = ("%%%ds" % n) + ": \t %6d (%3d%%); %6d (%3d%%); %6d (%3d%%) col; tot %d"
+    for k in keys:
+        co = hash_collisions[k]
+        eq = hash_equals[k]
+        ne = hash_notequals[k]
+        tot = hash_total[k]
+        sn, on = k
+        # Skip those that are all not equal
+        if sn != on and ne == tot:
+            continue
+        print(fmt % (k, eq, int(100.0*eq/tot),
+                            ne, int(100.0*ne/tot),
+                            co, int(100.0*co/tot),
+                            tot))
+
+
+def measure_collisions(equals_func):
+    def equals_func_with_collision_measuring(self, other):
+        # Call equals
+        equal = equals_func(self, other)
+
+        # Get properties
+        st = type(self)
+        ot = type(other)
+        sn = st.__name__
+        on = ot.__name__
+        sh = hash(self)
+        oh = hash(other)
+        key = (sn, on)
+
+        # If hashes are the same but objects are not equal, we have a collision
+        hash_total[key] += 1
+        if sh == oh and not equal:
+            hash_collisions[key] += 1
+        elif sh != oh and equal:
+            error("Equal objects must always have the same hash! Objects are:\n{0}\n{1}".format(self, other))
+        elif sh == oh and equal:
+            hash_equals[key] += 1
+        elif sh != oh and not equal:
+            hash_notequals[key] += 1
+
+        return equal
+    return equals_func_with_collision_measuring
+
+
+#@measure_collisions
+def recursive_expr_equals(self, other): # Much faster than the more complex algorithms above!
     """Checks whether the two expressions are represented the
     exact same way. This does not check if the expressions are
     mathematically equal or equivalent! Used by sets and dicts."""
 
-    # Code for counting number of equals calls:
-    #equalscalls[type(self)] = equalscalls.get(type(self),0) + 1
-
-    # Fast cutoff for common case
-    if type(self) != type(other):
+    # To handle expr == int/float
+    if not isinstance(other, Expr):
         return False
 
-    # TODO: Test how this affects the run time:
-    # Compare hashes if hash is cached
-    # (NB! never access _hash directly, it may be computed on demand in __hash__)
-    if (hasattr(self, "_hash") and hash(self) != hash(other)):
+    # Fast cutoff for common case
+    if self._ufl_typecode_ != other._ufl_typecode_:
+        return False
+
+    # Compare hashes, will cutoff more or less all nonequal types
+    if hash(self) != hash(other):
         return False
 
     # Large objects are costly to compare with themselves
     if self is other:
         return True
 
-    if isinstance(self, Operator):
-        # Just let python handle the recursion
-        #equal = self.ufl_operands == other.ufl_operands
-        # Recurse manually to call _expr_equals3 directly without the class EQ overhead!
-        equal = all(_expr_equals3(a, b) for (a, b) in zip(self.ufl_operands, other.ufl_operands))
-    else:
-        # Compare terminal representations to include all terminal data
-        #equal = repr(self) == repr(other)
+    # Terminals
+    if self._ufl_is_terminal_:
         # Compare terminals with custom == to capture subclass overloading of __eq__
-        equal = self == other
+        return self == other
 
-    # At this point, self and other has the same hash, and equal _should_ be True...
-    # Code for measuring amount of collisions:
-    #if not equal:
-    #    collisions[type(self)] = collisions.get(type(self), 0) + 1
+    # --- Operators, most likely equal, below here is the costly part if it recurses through a large tree! ---
 
-    # Code for counting number of recursive calls:
-    #equalsrecursed[type(self)] = equalsrecursed.get(type(self),0) + 1
-
-    # Debugging check: (has been enabled for a long while without any fails as of nov. 30th 2012
-    #req = repr(self) == repr(other)
-    #if req != equal: # This may legally fail for test/trial functions from PyDOLFIN
-    #    print '\n'*3
-    #    print self
-    #    print other
-    #    print '\n'*3
-    #    error("INVALID COMPARISON!")
+    # Recurse manually to call expr_equals directly without the class EQ overhead!
+    equal = all(recursive_expr_equals(a, b) for (a, b) in zip(self.ufl_operands, other.ufl_operands))
 
     return equal
 
-expr_equals = _expr_equals3
+
+#@measure_collisions
+def nonrecursive_expr_equals(self, other):
+    """Checks whether the two expressions are represented the
+    exact same way. This does not check if the expressions are
+    mathematically equal or equivalent! Used by sets and dicts."""
+
+    # Fast cutoffs for common cases, type difference or
+    # hash difference will cutoff more or less all nonequal types
+    if type(self) != type(other) or hash(self) != hash(other):
+        return False
+
+    # Large objects are costly to compare with themselves
+    if self is other:
+        return True
+
+    # Modelled after pre_traversal to avoid recursion:
+    left = [(self, other)]
+    while left:
+        s, o = left.pop()
+
+        if self._ufl_is_terminal_:
+            # Compare terminals
+            if self != other:
+                return False
+        else:
+            # Delve into subtrees
+            so = s.ufl_operands
+            oo = o.ufl_operands
+            if len(so) != len(oo):
+                return False
+
+            for s, o in zip(so, oo):
+                # Fast cutoff for common case
+                if s._ufl_typecode_ != o._ufl_typecode_:
+                    return False
+                # Skip subtree if objects are the same
+                if s is o:
+                    continue
+                # Append subtree for further inspection
+                left.append((s, o))
+
+    # Equal if we get out of the above loop!
+    return True
+
+#expr_equals = recursive_expr_equals
+expr_equals = nonrecursive_expr_equals
