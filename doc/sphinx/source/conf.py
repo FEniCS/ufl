e0437ea4a7df987fa93b33481a321ba6ef31fed4
Garth N. Wells <gnw20@cam.ac.uk>
2015-11-05 11:55:54 +0000

    Add link to code from docs.

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
