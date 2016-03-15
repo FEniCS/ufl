30ed15ee6d5b2d5d32e5ace17295b47f9212c47c
Chris Richardson <chris@bpi.cam.ac.uk>
2016-03-15 09:47:57 +0000

    Fix case of non-mixed space, as suggested by Lawrence

--- ufl/formsplitter.py
+++ ufl/formsplitter.py
@@ -1,25 +1,49 @@
+# -*- coding: utf-8 -*-
+"Extract part of a form in a mixed FunctionSpace."
+
+# Copyright (C) 2016 Chris Richardson and Lawrence Mitchell
+#
+# This file is part of UFL.
+#
+# UFL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# UFL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with UFL. If not, see <http://www.gnu.org/licenses/>.
 
 from ufl.corealg.multifunction import MultiFunction
 from ufl.algorithms.map_integrands import map_integrand_dags
 from ufl.constantvalue import Zero
-from ufl import as_vector
-from ufl.algorithms import expand_derivatives
+from ufl.tensors import as_vector
+from ufl.argument import Argument
+from ufl.functionspace import FunctionSpace
 
 class FormSplitter(MultiFunction):
 
     def split(self, form, ix, iy=0):
-        # remember which block to extract
+        # Remember which block to extract
         self.idx = [ix, iy]
-        # FIXME: is this needed?
-#        form = expand_derivatives(form)
         return map_integrand_dags(self, form)
 
     def argument(self, obj):
         Q = obj.ufl_function_space()
         dom = Q.ufl_domain()
-        elem = obj.ufl_element()
+        sub_elements = obj.ufl_element().sub_elements()
+
+        # If not a mixed element, do nothing
+        if (len(sub_elements) == 0):
+            return obj
+
+        # Split into sub-elements, creating appropriate space for each
         args = []
-        for i, sub_elem in enumerate(elem.sub_elements()):
+        for i, sub_elem in enumerate(sub_elements):
             Q_i = FunctionSpace(dom, sub_elem)
             a = Argument(Q_i, obj.number(), part=obj.part())
 
@@ -31,9 +55,14 @@ class FormSplitter(MultiFunction):
                 args += [a[j] for j in indices]
             else:
                 args += [Zero() for j in indices]
+
         return as_vector(args)
 
     def multi_index(self, obj):
         return obj
 
     expr = MultiFunction.reuse_if_untouched
+
+def block_split(form, ix, iy=0):
+    fs = FormSplitter()
+    return fs.split(form, ix, iy)
