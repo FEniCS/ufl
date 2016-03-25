0ef5fe736c159c9c90b9623dfab2474f44158dd8
David Ham <David.Ham@imperial.ac.uk>
2016-03-25 21:43:20 +0100

    fix __new__ for Python 3

--- ufl/finiteelement/finiteelement.py
+++ ufl/finiteelement/finiteelement.py
@@ -31,6 +31,7 @@ from ufl.cell import TensorProductCell
 from ufl.finiteelement.elementlist import canonical_element_description, simplices
 from ufl.finiteelement.finiteelementbase import FiniteElementBase
 
+
 class FiniteElement(FiniteElementBase):
     "The basic finite element class for all simple finite elements"
     # TODO: Move these to base?
@@ -52,7 +53,7 @@ class FiniteElement(FiniteElementBase):
             cell = as_cell(cell)
 
         family, short_name, degree, value_shape, reference_value_shape, sobolev_space, mapping = \
-          canonical_element_description(family, cell, degree, form_degree)
+            canonical_element_description(family, cell, degree, form_degree)
 
         if isinstance(cell, TensorProductCell):
             # Delay import to avoid circular dependency at module load time
@@ -103,22 +104,17 @@ class FiniteElement(FiniteElementBase):
 
             elif family == "Q":
                 return TensorProductElement(FiniteElement("CG", cell._cells[0], degree, 0, quad_scheme),
-                                           FiniteElement("CG", cell._cells[1], degree, 0, quad_scheme),
-                                           cell)
+                                            FiniteElement("CG", cell._cells[1], degree, 0, quad_scheme),
+                                            cell)
 
             elif family == "DQ":
                 family_A = "DG" if cell._cells[0].cellname() in simplices else "DQ"
                 family_B = "DG" if cell._cells[1].cellname() in simplices else "DQ"
                 return TensorProductElement(FiniteElement(family_A, cell._cells[0], degree, cell._cells[0].topological_dimension(), quad_scheme),
-                                           FiniteElement(family_B, cell._cells[1], degree, cell._cells[1].topological_dimension(), quad_scheme),
-                                           cell)
+                                            FiniteElement(family_B, cell._cells[1], degree, cell._cells[1].topological_dimension(), quad_scheme),
+                                            cell)
 
-        return super(FiniteElement, cls).__new__(cls,
-                                                 family,
-                                                 cell,
-                                                 degree,
-                                                 form_degree,
-                                                 quad_scheme)
+        return super(FiniteElement, cls).__new__(cls)
 
     def __init__(self,
                  family,
