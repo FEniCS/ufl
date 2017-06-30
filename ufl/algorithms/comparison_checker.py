ef764f897cbca458a88926d84fee4e82a757826a
NicholasBermuda <snicholasbarton@gmail.com>
2017-06-30 23:23:55 +0100

    comparison checker progress

--- ufl/algorithms/comparison_checker.py
+++ ufl/algorithms/comparison_checker.py
@@ -6,17 +6,19 @@ from ufl.corealg.multifunction import MultiFunction
 from ufl.algorithms.map_integrands import map_integrand_dags
 from ufl.algebra import Real
 
-class CheckInvalidComparisons(MultiFunction):
+class CheckComparisons(MultiFunction):
     """Raises an error if comparisons are done with complex quantities.
 
     If quantities are real, adds the Real operator to the compared quantities.
+
+    Also removes unnecessary Conj of real quantities. 
 
     Quantities that are real are Abs, Real, Imag.
     Terminals default to complex, and Sqrt, Pow (defensively) imply complex"""
     def __init__(self):
         MultiFunction.__init__(self)
 
-    expr = reuse_and_check_type
+    expr = MultiFunction.reuse_and_check_type
 
     def gt(self, o, *ops):
         ops, types = zip(*ops)
@@ -50,48 +52,51 @@ class CheckInvalidComparisons(MultiFunction):
         else:
             return o._ufl_expr_reconstruct_(*map(Real, *ops))
 
-    def real(self, o, *ops):
-
-    def imag(self, o, *ops):
-
-    def sqrt(self, o, *ops):
-
-    def pow(self, o, *ops):
-
-    def abs(self, o, *ops):
-
-    def terminal():
-        # IntValue FloatValue Abs --> real
-        # Sqrt, Pow --> imag (because we dont know the value of what's inside)
-        # something for max and min also??
-
-def reuse_and_check_type(self, o, *ops):
-        """Reuse object if operands are the same objects, and
-        check the type of the operands.
-
-        Use in your own subclass by setting e.g.
-        ::
-
-            expr = reuse_and_check_type
-
-        as a default rule.
-        """
+    def max(self, o, *ops):
         ops, types = zip(*ops)
 
-        if types:
-            t = "complex" if "complex" in types else "real"
+        if "complex" in types:
+            raise ComplexComparisonError("You can't order complex numbers with max.")
         else:
-            # Default terminals to Complex
-            t = t or "complex"
+            return o._ufl_expr_reconstruct_(*map(Real, *ops))
 
-        if all(a is b for a, b in zip(o.ufl_operands, ops)):
-            return (o, t)
+    def min(self, o, *ops):
+        ops, types = zip(*ops)
+
+        if "complex" in types:
+            raise ComplexComparisonError("You can't order complex numbers with min.")
         else:
-            return (o._ufl_expr_reconstruct_(*ops), t)
+            return o._ufl_expr_reconstruct_(*map(Real, *ops))
+
+    def conj(self, o, *ops):
+        ops, types = zip(*ops)
+
+        if "complex" in types:
+            print('found a complex')
+            return o
+        else:
+            print('found a real')
+            return o._ufl_expr_reconstruct_(*map(Real, *ops))
+
+    # def real(self, o, *ops):
+
+    # def imag(self, o, *ops):
+
+    # def sqrt(self, o, *ops):
+
+    # def pow(self, o, *ops):
+
+    # def abs(self, o, *ops):
+
+    # def terminal(self, o, *ops):
+    #     # IntValue FloatValue Abs --> real
+    #     # Sqrt, Pow --> imag (because we dont know the value of what's inside)
+    #     # something for max and min also??
+
 
 def do_comparison_check(expr):
     """Raises an error if comparison nodes exist"""
-    return map_integrand_dags(CheckInvalidComparisons, expr)
+    return map_integrand_dags(CheckComparisons, expr)
 
 class ComplexComparisonError(Exception):
     pass
