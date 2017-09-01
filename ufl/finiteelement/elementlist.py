83292f5b939325ea25e84f866c5d50f9fa711c87
Miklos Homolya <m.homolya14@imperial.ac.uk>
2017-09-01 17:34:39 +0100

    WIP: accept all degrees 3 or greater for Hermite

--- ufl/finiteelement/elementlist.py
+++ ufl/finiteelement/elementlist.py
@@ -134,7 +134,7 @@ register_element("Crouzeix-Raviart", "CR", 0, L2, "identity", (1, 1),
 # TODO: Implement generic Tear operator for elements instead of this:
 register_element("Discontinuous Raviart-Thomas", "DRT", 1, L2,
                  "contravariant Piola", (1, None), simplices[1:])
-register_element("Hermite", "HER", 0, H1, "identity", None, simplices[1:])
+register_element("Hermite", "HER", 0, H1, "identity", (3, None), simplices[1:])
 register_element("Mardal-Tai-Winther", "MTW", 0, H1, "identity", None,
                  ("triangle",))
 register_element("Morley", "MOR", 0, H2, "identity", None, ("triangle",))
