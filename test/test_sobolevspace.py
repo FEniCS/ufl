866aa7ed76d402d0d433841fda2730bde457b4b5
Rob Kirby <robert.c.kirby@gmail.com>
2020-08-18 13:03:05 -0500

    Fix test

--- test/test_sobolevspace.py
+++ test/test_sobolevspace.py
@@ -94,7 +94,6 @@ def test_contains_h1():
         FiniteElement("CG", triangle, 1),
         FiniteElement("CG", triangle, 2),
         # Some special elements:
-        FiniteElement("AW", triangle),
         FiniteElement("HER", triangle),
         FiniteElement("MTW", triangle),
         # Tensor product elements:
@@ -138,6 +137,7 @@ def test_contains_hdiv():
         FiniteElement("RT", triangle, 1),
         FiniteElement("BDM", triangle, 1),
         FiniteElement("BDFM", triangle, 2),
+        
         # HDiv elements:
         HDiv(TensorProductElement(FiniteElement("DG", triangle, 1),
                                   FiniteElement("CG", interval, 2))),
@@ -145,6 +145,7 @@ def test_contains_hdiv():
                                   FiniteElement("DG", interval, 1))),
         HDiv(TensorProductElement(FiniteElement("N1curl", triangle, 1),
                                   FiniteElement("DG", interval, 1)))
+
     ]
     for hdiv_element in hdiv_elements:
         assert hdiv_element in HDiv
