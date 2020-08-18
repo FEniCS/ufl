bc83effea61e385221c8d61ca5cb448725d56131
Rob Kirby <robert.c.kirby@gmail.com>
2020-08-18 12:53:18 -0500

    flake8

--- ufl/finiteelement/elementlist.py
+++ ufl/finiteelement/elementlist.py
@@ -124,7 +124,7 @@ register_element("Crouzeix-Raviart", "CR", 0, L2, "identity", (1, 1),
 register_element("Discontinuous Raviart-Thomas", "DRT", 1, L2,
                  "contravariant Piola", (1, None), simplices[1:])
 register_element("Hermite", "HER", 0, H1, "identity", (3, 3), simplices)
-register_element("Mardal-Tai-Winther", "MTW", 1, H1, "contravariant Piola", (3,3) ,
+register_element("Mardal-Tai-Winther", "MTW", 1, H1, "contravariant Piola", (3, 3),
                  ("triangle",))
 register_element("Morley", "MOR", 0, H2, "identity", (2, 2), ("triangle",))
 
