e88ec05be4c8f4d421003a5464f082b3dab9ade5
keith roberts <krober@usp.br>
2020-09-11 13:04:26 -0300

    Update elementlist.py

--- ufl/finiteelement/elementlist.py
+++ ufl/finiteelement/elementlist.py
@@ -124,7 +124,7 @@ register_element("Crouzeix-Raviart", "CR", 0, L2, "identity", (1, 1),
 register_element("Discontinuous Raviart-Thomas", "DRT", 1, L2,
                  "contravariant Piola", (1, None), simplices[1:])
 register_element("Hermite", "HER", 0, H1, "identity", (3, 3), simplices)
-register_element("KongMulderVeldhuizen", "KMV", 0, H1, "identity", (1, None),
+register_element("Kong-Mulder-Veldhuizen", "KMV", 0, H1, "identity", (1, None),
                  simplices[1:])
 register_element("Mardal-Tai-Winther", "MTW", 0, H1, "identity", None,
                  ("triangle",))
