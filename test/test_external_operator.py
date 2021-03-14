006f9bbf3514f4a3cea138640bc671ea87d863b3
nbouziani <nacime.bouziani@gmail.com>
2021-03-14 13:03:25 +0000

    Change some tests

--- test/test_external_operator.py
+++ test/test_external_operator.py
@@ -247,9 +247,11 @@ def test_dependency():
     expand_derivatives(Ja)
     de3 = tuple(e3.coefficient_dict.values())
 
-    assert sorted(e3.coefficient_dict.keys()) == [(1,), (2,)]
-    assert de3[0]._extop_master == e3
-    assert de3[1]._extop_master == e3
+    assert sorted(e3.coefficient_dict.keys()) == []
+    assert de3 == ()
+    # assert sorted(e3.coefficient_dict.keys()) == [(1,), (2,)]
+    # assert de3[0]._extop_master == e3
+    # assert de3[1]._extop_master == e3
 
     e4 = ExternalOperator(u, function_space=Vv)
 
@@ -258,9 +260,11 @@ def test_dependency():
     expand_derivatives(Ja)
     de4 = tuple(e4.coefficient_dict.values())
 
-    assert sorted(e4.coefficient_dict.keys()) == [(1,), (2,)]
-    assert de4[0]._extop_master == e4
-    assert de4[1]._extop_master == e4
+    assert sorted(e4.coefficient_dict.keys()) == []
+    assert de4 == ()
+    # assert sorted(e4.coefficient_dict.keys()) == [(1,), (2,)]
+    # assert de4[0]._extop_master == e4
+    # assert de4[1]._extop_master == e4
 
 
 def test_function_spaces_derivatives():
