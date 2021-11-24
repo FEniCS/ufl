1b44c18c8ad1e9a68486a4f02f17eb0439a31954
nbouziani <nacime.bouziani@gmail.com>
2021-11-24 19:41:39 +0000

    Fix _analyze_form_arguments for FormSum

--- ufl/form.py
+++ ufl/form.py
@@ -739,8 +739,9 @@ class FormSum(BaseForm):
         "Return all ``Argument`` objects found in form."
         arguments = []
         for component in self._components:
-            arguments.append(component.arguments())
-        self._arguments = arguments
+            arguments.extend(component.arguments())
+        # Remove duplicates
+        self._arguments = tuple(set(arguments))
 
     def __hash__(self):
         "Hash code for use in dicts (includes incidental numbering of indices etc.)"
