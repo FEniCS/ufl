2da724369b239f71faef56a45434b1f3bc663d9a
nbouziani <nacime.bouziani@gmail.com>
2021-11-24 20:04:32 +0000

    Fix ExprList

--- ufl/exprcontainers.py
+++ ufl/exprcontainers.py
@@ -11,6 +11,7 @@ from ufl.log import error
 from ufl.core.expr import Expr
 from ufl.core.operator import Operator
 from ufl.core.ufl_type import ufl_type
+from ufl.coefficient import Cofunction
 from ufl.argument import Coargument
 
 
@@ -23,9 +24,9 @@ class ExprList(Operator):
 
     def __init__(self, *operands):
         Operator.__init__(self, operands)
-        # Enable Coargument for BaseForm differentiation (argument lives in a dual space)
-        if not all(isinstance(i, (Expr, Coargument)) for i in operands):
-            error("Expecting Expr or Coargument in ExprList.")
+        # Enable Cofunction/Coargument for BaseForm differentiation
+        if not all(isinstance(i, (Expr, Cofunction, Coargument)) for i in operands):
+            error("Expecting Expr, Cofunction or Coargument in ExprList.")
 
     def __getitem__(self, i):
         return self.ufl_operands[i]
