4aff089d8febb2c6bc0cb4ff59f877d402a8c08b
nbouziani <nacime.bouziani@gmail.com>
2021-11-25 13:30:17 +0000

    Update test

--- test/test_interp.py
+++ test/test_interp.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env py.test
 # -*- coding: utf-8 -*-
 
-from ufl import FiniteElement, FunctionSpace, Coefficient, Argument, triangle
+import pytest
+from ufl import FiniteElement, FunctionSpace, Coefficient, Argument, triangle, derivative
 
 __authors__ = "Nacime Bouziani"
 __date__ = "2021-11-19"
@@ -10,19 +11,28 @@ __date__ = "2021-11-19"
 Test Interp object
 """
 
+from ufl.algorithms.ad import expand_derivatives
 from ufl.core.interp import Interp
 from ufl.domain import default_domain
 
 
-def test_symbolic():
-
-    # -- Set function spaces -- #
+@pytest.fixture
+def V1():
     domain_2d = default_domain(triangle)
     f1 = FiniteElement("CG", triangle, 1)
-    V1 = FunctionSpace(domain_2d, f1)
+    return FunctionSpace(domain_2d, f1)
 
-    f2 = FiniteElement("CG", triangle, 2)
-    V2 = FunctionSpace(domain_2d, f2)
+
+@pytest.fixture
+def V2():
+    domain_2d = default_domain(triangle)
+    f1 = FiniteElement("CG", triangle, 2)
+    return FunctionSpace(domain_2d, f1)
+
+
+def test_symbolic(V1, V2):
+
+    # Set dual of V2
     V2_dual = V2.dual()
 
     u = Coefficient(V1)
@@ -35,23 +45,10 @@ def test_symbolic():
     assert Iu.arguments() == (vstar,)
     assert Iu.ufl_operands == ()
 
-    # Check trivial cases
-    I = Interp(u, V1)
-    assert I == u
 
-    I = Interp(0, V1)
-    assert I == 0
+def test_action_adjoint(V1, V2):
 
-
-def test_action_adjoint():
-
-    # -- Set function spaces -- #
-    domain_2d = default_domain(triangle)
-    f1 = FiniteElement("CG", triangle, 1)
-    V1 = FunctionSpace(domain_2d, f1)
-
-    f2 = FiniteElement("CG", triangle, 2)
-    V2 = FunctionSpace(domain_2d, f2)
+    # Set dual of V2
     V2_dual = V2.dual()
 
     # u = Coefficient(V1)
@@ -67,8 +64,18 @@ def test_action_adjoint():
     # -- Add tests for Action/Adjoint -- #
 
 
-"""
-def test_differentiation():
+def test_differentiation(V1, V2):
 
-    # Put differentiation equivalence: dInterp(u, v*)/du[uhat] <==> Interp(uhat, v*)
-"""
+    u = Coefficient(V1)
+    Iu = Interp(u, V2)
+
+    v1 = Argument(V1, 1)
+    dIu = expand_derivatives(derivative(Iu, u, v1))
+
+    # dInterp(u, v*)/du[uhat] <==> Interp(uhat, v*)
+    assert dIu == Interp(v1, V2)
+
+    g = u**2
+    Ig = Interp(g, V2)
+    dIg = expand_derivatives(derivative(Ig, u, v1))
+    assert dIg == Interp(2 * v1 * u, V2)
