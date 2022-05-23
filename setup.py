b8df1e1b79ace47697fd1fa1134b1b1ce7b05faf
Jørgen Dokken <dokken92@gmail.com>
2022-05-23 15:24:06 +0000

    Update text

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ try:
 
     from packaging import version
     if version.parse(pip.__version__) < version.parse("21.3"):
-        # Can be removed when pip editable user installs are fixed
-        # https://github.com/pypa/pip/issues/7953
+        # Issue with older version of pip https://github.com/pypa/pip/issues/7953
         import site
         import sys
         site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
