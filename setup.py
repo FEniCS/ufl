67266086eadd5b29cde5f7566ef2a9369eac12ce
Jørgen Dokken <dokken92@gmail.com>
2022-05-23 15:19:48 +0000

    Use packaging version to check for correct version of pip

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 import setuptools
-import sys
 
+try:
+    import pip
 
-if sys.version_info < (3, 10):
-    # Can be removed when pip editable user installs are fixed
-    # https://github.com/pypa/pip/issues/7953
-    import site
-    site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+    from packaging import version
+    if version.parse(pip.__version__) < version.parse("21.3"):
+        # Can be removed when pip editable user installs are fixed
+        # https://github.com/pypa/pip/issues/7953
+        import site
+        import sys
+        site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+except ImportError:
+    pass
 
 setuptools.setup()
