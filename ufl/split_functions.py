32da753b7a859aa26aaddb142ac733df92ade42c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-09-26 20:29:48 +0100

    Use num_sub_elements instead of isinstance(, MixedElement)

--- ufl/split_functions.py
+++ ufl/split_functions.py
@@ -51,7 +51,7 @@ def split(v):
 
     # Special case: simple element, just return function in a tuple
     element = v.ufl_element()
-    if not isinstance(element, MixedElement):
+    if element.num_sub_elements == 0:
         assert end is None
         return (v,)
 
