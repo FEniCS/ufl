b458b13ab1b13e102189fafbd5d873418f2ac1bc
Pablo Brubeck <brubeck@protonmail.com>
2022-10-17 10:14:15 +0100

    InteriorElement in L2

--- ufl/finiteelement/restrictedelement.py
+++ ufl/finiteelement/restrictedelement.py
@@ -40,10 +40,10 @@ class RestrictedElement(FiniteElementBase):
         return f"RestrictedElement({repr(self._element)}, {repr(self._restriction_domain)})"
 
     def sobolev_space(self):
-        if self._restriction_domain == "facet":
-            return self._element.sobolev_space()
-        else:
+        if self._restriction_domain == "interior":
             return L2
+        else:
+            return self._element.sobolev_space()
 
     def is_cellwise_constant(self):
         """Return whether the basis functions of this element is spatially
