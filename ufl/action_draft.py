ef6d2c889cfd6caa7ac11d03b613437a36bf9dc5
nbouziani <nacime.bouziani@gmail.com>
2022-12-20 14:17:56 +0000

    Backup

---
+++ ufl/action_draft.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+"""This module defines the Action class."""
+
+# Copyright (C) 2021 India Marsden
+#
+# This file is part of UFL (https://www.fenicsproject.org)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+#
+# Modified by Nacime Bouziani, 2021-2022.
+
+from ufl.form import BaseForm, FormSum, Form, ZeroBaseForm
+from ufl.core.ufl_type import ufl_type
+from ufl.algebra import Sum
+from ufl.constantvalue import Zero
+from ufl.argument import Argument
+from ufl.coefficient import BaseCoefficient, Coefficient, Cofunction
+from ufl.differentiation import CoefficientDerivative
+from ufl.core.base_form_operator import BaseFormOperator
+from ufl.matrix import Matrix
+
+# --- The Action class represents the action of a numerical object that needs
+#     to be computed at assembly time ---
+
+
+@ufl_type()
+class Action(BaseForm):
+    """UFL base form type: respresents the action of an object on another.
+    For example:
+        res = Ax
+    A would be the first argument, left and x would be the second argument,
+    right.
+
+    Action objects will result when the action of an assembled object
+    (e.g. a Matrix) is taken. This delays the evaluation of the action until
+    assembly occurs.
+    """
+
+    __slots__ = (
+        "_left",
+        "_right",
+        "ufl_operands",
+        "_repr",
+        "_arguments",
+        "_coefficients",
+        "_hash")
+
+    def __getnewargs__(self):
+        return (self._left, self._right)
+
+    def __new__(cls, *args, **kw):
+        left, right = args
+
+        # Check trivial case
+        if left == 0 or right == 0:
+            # Check compatibility of function spaces
+            _check_function_spaces(left, right)
+            # Still need to work out the ZeroBaseForm arguments.
+            new_arguments, _ = _get_action_form_arguments(left, right)
+            return ZeroBaseForm(new_arguments)
+
+        # Check trivial case
+        if left == 0 or right == 0:
+            return 0
+
+        if isinstance(left, (FormSum, Sum)):
+            # Action distributes over sums on the LHS
+            return FormSum(*[(Action(component, right), 1)
+                             for component in left.ufl_operands])
+        if isinstance(right, (FormSum, Sum)):
+            # Action also distributes over sums on the RHS
+            return FormSum(*[(Action(left, component), 1)
+                             for component in right.ufl_operands])
+
+        return super(Action, cls).__new__(cls)
+
+    def __init__(self, left, right):
+        BaseForm.__init__(self)
+
+        self._left = left
+        self._right = right
+        self.ufl_operands = (self._left, self._right)
+
+        # Check compatibility of function spaces
+        _check_function_spaces(left, right)
+
+        self._repr = "Action(%s, %s)" % (repr(self._left), repr(self._right))
+        self._hash = None
+
+    def ufl_function_spaces(self):
+        "Get the tuple of function spaces of the underlying form"
+        if isinstance(self._right, Form):
+            return self._left.ufl_function_spaces()[:-1] \
+                + self._right.ufl_function_spaces()[1:]
+        elif isinstance(self._right, Coefficient):
+            return self._left.ufl_function_spaces()[:-1]
+
+    def left(self):
+        return self._left
+
+    def right(self):
+        return self._right
+
+    def _analyze_form_arguments(self):
+        """Compute the Arguments of this Action.
+
+        The highest number Argument of the left operand and the lowest number
+        Argument of the right operand are consumed by the action.
+        """
+        self._arguments, self._coefficients = _get_action_form_arguments(self._left, self._right)
+
+    def equals(self, other):
+        if type(other) is not Action:
+            return False
+        if self is other:
+            return True
+        return (self._left == other._left and self._right == other._right)
+
+    def __eq__(self, other):
+        if not isinstance(other, Action):
+            return False
+        if self is other:
+            return True
+        return (self._left == other._left and self._right == other._right)
+
+    def __str__(self):
+        return "Action(%s, %s)" % (str(self._left), str(self._right))
+
+    def __repr__(self):
+        return self._repr
+
+    def __hash__(self):
+        "Hash code for use in dicts "
+        if self._hash is None:
+            self._hash = hash(("Action",
+                               hash(self._right),
+                               hash(self._left)))
+        return self._hash
+
+
+def _check_function_spaces(left, right):
+    "Check if the function spaces of left and right match."
+
+    if isinstance(right, CoefficientDerivative):
+        # Action differentiation pushes differentiation through
+        # right as a consequence of Leibniz formula.
+        right, *_ = right.ufl_operands
+
+    # `Zero` doesn't contain any information about the function space.
+    # -> Not a problem since Action will get simplified with a `ZeroBaseForm`
+    #    which won't take into account the arguments on the right because of argument contraction.
+    # This occurs for:
+    # `Action(A, derivative(B, u))` where B is a `BaseFormOperator` (with 1 argument) such that dB/du == 0
+    # -> `derivative(B, u)` becomes `Zero` when expanding derivatives since B is an Expr as well.
+    if not isinstance(left, Zero) and not isinstance(right, Zero):
+        if isinstance(right, (Form, Action, Matrix, ZeroBaseForm)):
+            if (left.arguments()[-1].ufl_function_space().dual()
+                != right.arguments()[0].ufl_function_space()):
+
+                raise TypeError("Incompatible function spaces in Action")
+        elif isinstance(right, (Coefficient, Cofunction, Argument, BaseFormOperator)):
+            if (left.arguments()[-1].ufl_function_space()
+                != right.ufl_function_space()):
+
+                raise TypeError("Incompatible function spaces in Action")
+        else:
+            raise TypeError("Incompatible argument in Action: %s" % type(right))
+
+
+def _get_action_form_arguments(left, right):
+    "Perform argument contraction to work out the arguments of Action"
+
+    if isinstance(right, CoefficientDerivative):
+        # Action differentiation pushes differentiation through
+        # right as a consequence of Leibniz formula.
+        right, *_ = right.ufl_operands
+
+    left_args = left.arguments()[:-1] if not isinstance(left, Zero) else () 
+    coefficients = ()
+    if isinstance(right, BaseForm):
+        arguments = left_args + right.arguments()[1:]
+        coefficients += right.coefficients()
+    elif isinstance(right, (BaseCoefficient, Zero)):
+        arguments = left_args
+        # For `Zero` case, Action gets simplified so updating
+        # coefficients here doesn't matter
+        coefficients += (right,)
+    elif isinstance(right, Argument):
+        arguments = left_args + (right,)
+    else:
+        raise TypeError
+
+    if isinstance(left, BaseForm):
+        coefficients += left.coefficients()
+
+    return arguments, coefficients
---
+++ ufl/adjoint_draft.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""This module defines the Adjoint class."""
+
+# Copyright (C) 2021 India Marsden
+#
+# This file is part of UFL (https://www.fenicsproject.org)
+#
+# SPDX-License-Identifier:    LGPL-3.0-or-later
+#
+# Modified by Nacime Bouziani, 2021-2022.
+
+from ufl.form import BaseForm, FormSum, ZeroBaseForm
+from ufl.core.ufl_type import ufl_type
+# --- The Adjoint class represents the adjoint of a numerical object that
+#     needs to be computed at assembly time ---
+
+
+@ufl_type()
+class Adjoint(BaseForm):
+    """UFL base form type: represents the adjoint of an object.
+
+    Adjoint objects will result when the adjoint of an assembled object
+    (e.g. a Matrix) is taken. This delays the evaluation of the adjoint until
+    assembly occurs.
+    """
+
+    __slots__ = (
+        "_form",
+        "_repr",
+        "_arguments",
+        "_coefficients",
+        "ufl_operands",
+        "_hash")
+
+    def __getnewargs__(self):
+        return (self._form)
+
+    def __new__(cls, *args, **kw):
+        form = args[0]
+        # Check trivial case: This is not a ufl.Zero but a ZeroBaseForm!
+        if form == 0:
+            if isinstance(form, BaseForm):
+                # Swap the arguments
+                return ZeroBaseForm(form.arguments()[::-1])
+            # For example if form is ufl.Zero
+            return form
+
+        if isinstance(form, Adjoint):
+            return form._form
+        elif isinstance(form, FormSum):
+            # Adjoint distributes over sums
+            return FormSum(*[(Adjoint(component), 1)
+                             for component in form.components()])
+
+        return super(Adjoint, cls).__new__(cls)
+
+    def __init__(self, form):
+        BaseForm.__init__(self)
+
+        if len(form.arguments()) != 2:
+            raise ValueError("Can only take Adjoint of a 2-form.")
+
+        self._form = form
+        self.ufl_operands = (self._form,)
+        self._hash = None
+        self._repr = "Adjoint(%s)" % repr(self._form)
+
+    def ufl_function_spaces(self):
+        "Get the tuple of function spaces of the underlying form"
+        return self._form.ufl_function_spaces()
+
+    def form(self):
+        return self._form
+
+    def _analyze_form_arguments(self):
+        """The arguments of adjoint are the reverse of the form arguments."""
+        self._arguments = self._form.arguments()[::-1]
+        self._coefficients = self._form.coefficients()
+
+    def equals(self, other):
+        if type(other) is not Adjoint:
+            return False
+        if self is other:
+            return True
+        return (self._form == other._form)
+
+    def __str__(self):
+        return "Adjoint(%s)" % str(self._form)
+
+    def __repr__(self):
+        return self._repr
+
+    def __hash__(self):
+        """Hash code for use in dicts."""
+        if self._hash is None:
+            self._hash = hash(("Adjoint", hash(self._form)))
+        return self._hash
