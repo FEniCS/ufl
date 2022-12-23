e85eecd42d6e02503181b60fb4e0a8e4b460a88c
Jørgen S. Dokken <dokken@simula.no>
2022-12-23 15:53:07 +0100

    Remove asserts

--- ufl/core/expr.py
+++ ufl/core/expr.py
@@ -298,7 +298,6 @@ class Expr(object):
         warnings.warn("Expr.ufl_domains() is deprecated, please "
                       "use extract_domains(expr) instead.", DeprecationWarning)
         from ufl.domain import extract_domains
-        assert False
         return extract_domains(self)
 
     def ufl_domain(self):
@@ -306,7 +305,6 @@ class Expr(object):
         warnings.warn("Expr.ufl_domain() is deprecated, please "
                       "use extract_unique_domain(expr) instead.", DeprecationWarning)
         from ufl.domain import extract_unique_domain
-        assert False
         return extract_unique_domain(self)
 
     # --- Functions for float evaluation ---
