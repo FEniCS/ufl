e4fc5bcada9e299ba244f6b13bb6f2c11d30fc31
nbouziani <nacime.bouziani@gmail.com>
2023-01-13 00:44:59 +0000

    Revert "Revert "Allow multiple subdomain data (#120)""
    This reverts commit 073579e6a0ec68445776e6d9606827d760e66bd6.

--- test/test_measures.py
+++ test/test_measures.py
@@ -109,26 +109,26 @@ def test_foo():
     # domain=None,
     # subdomain_id="everywhere",
     # metadata=None)
-    assert dx.ufl_domain() == None
+    assert dx.ufl_domain() is None
     assert dx.subdomain_id() == "everywhere"
 
     # Set subdomain_id to "everywhere", still no domain set
     dxe = dx()
-    assert dxe.ufl_domain() == None
+    assert dxe.ufl_domain() is None
     assert dxe.subdomain_id() == "everywhere"
 
     # Set subdomain_id to 5, still no domain set
     dx5 = dx(5)
-    assert dx5.ufl_domain() == None
+    assert dx5.ufl_domain() is None
     assert dx5.subdomain_id() == 5
 
     # Check that original dx is untouched
-    assert dx.ufl_domain() == None
+    assert dx.ufl_domain() is None
     assert dx.subdomain_id() == "everywhere"
 
     # Set subdomain_id to (2,3), still no domain set
     dx23 = dx((2, 3))
-    assert dx23.ufl_domain() == None
+    assert dx23.ufl_domain() is None
     assert dx23.subdomain_id(), (2 == 3)
 
     # Map metadata to metadata, ffc interprets as before
@@ -136,7 +136,7 @@ def test_foo():
     # assert dxm.metadata() == {"dummy":123}
     assert dxm.metadata() == {"dummy": 123}  # Deprecated, TODO: Remove
 
-    assert dxm.ufl_domain() == None
+    assert dxm.ufl_domain() is None
     assert dxm.subdomain_id() == "everywhere"
 
     # dxm = dx(metadata={"dummy":123})
@@ -144,7 +144,7 @@ def test_foo():
     dxm = dx(metadata={"dummy": 123})
     assert dxm.metadata() == {"dummy": 123}
 
-    assert dxm.ufl_domain() == None
+    assert dxm.ufl_domain() is None
     assert dxm.subdomain_id() == "everywhere"
 
     dxi = dx(metadata={"quadrature_degree": 3})
@@ -163,9 +163,9 @@ def test_foo():
     dSd = dS(subdomain_data=interior_facet_domains)
     # Current behaviour: no domain created, measure domain data is a single
     # object not a full dict
-    assert dxd.ufl_domain() == None
-    assert dsd.ufl_domain() == None
-    assert dSd.ufl_domain() == None
+    assert dxd.ufl_domain() is None
+    assert dsd.ufl_domain() is None
+    assert dSd.ufl_domain() is None
     assert dxd.subdomain_data() is cell_domains
     assert dsd.subdomain_data() is exterior_facet_domains
     assert dSd.subdomain_data() is interior_facet_domains
@@ -180,23 +180,25 @@ def test_foo():
     domain, = Mx.ufl_domains()
     assert domain.ufl_id() == mydomain.ufl_id()
     assert domain.ufl_cargo() == mymesh
-    assert Mx.subdomain_data()[mydomain]["cell"] == cell_domains
+    assert len(Mx.subdomain_data()[mydomain]["cell"]) == 1
+    assert Mx.subdomain_data()[mydomain]["cell"][0] == cell_domains
 
     domain, = Ms.ufl_domains()
     assert domain.ufl_cargo() == mymesh
-    assert Ms.subdomain_data()[mydomain][
-        "exterior_facet"] == exterior_facet_domains
+    assert len(Ms.subdomain_data()[mydomain]["exterior_facet"]) == 1
+    assert Ms.subdomain_data()[mydomain]["exterior_facet"][0] == exterior_facet_domains
 
     domain, = MS.ufl_domains()
     assert domain.ufl_cargo() == mymesh
-    assert MS.subdomain_data()[mydomain][
-        "interior_facet"] == interior_facet_domains
+    assert len(MS.subdomain_data()[mydomain]["interior_facet"]) == 1
+    assert MS.subdomain_data()[mydomain]["interior_facet"][0] == interior_facet_domains
 
     # Test joining of these domains in a single form
     domain, = M.ufl_domains()
     assert domain.ufl_cargo() == mymesh
-    assert M.subdomain_data()[mydomain]["cell"] == cell_domains
-    assert M.subdomain_data()[mydomain][
-        "exterior_facet"] == exterior_facet_domains
-    assert M.subdomain_data()[mydomain][
-        "interior_facet"] == interior_facet_domains
+    assert len(M.subdomain_data()[mydomain]["cell"]) == 1
+    assert M.subdomain_data()[mydomain]["cell"][0] == cell_domains
+    assert len(M.subdomain_data()[mydomain]["exterior_facet"]) == 1
+    assert M.subdomain_data()[mydomain]["exterior_facet"][0] == exterior_facet_domains
+    assert len(M.subdomain_data()[mydomain]["interior_facet"]) == 1
+    assert M.subdomain_data()[mydomain]["interior_facet"][0] == interior_facet_domains
