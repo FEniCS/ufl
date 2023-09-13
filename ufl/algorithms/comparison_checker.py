fa613eb81a739478699a541a68a8c8a72119cdbf
India Marsden <imm1117@ic.ac.uk>
2023-09-13 10:22:32 +0100

    Convert comparision checker to single dispatch - multifunction implementatin still there due to issues with sign

--- ufl/algorithms/comparison_checker.py
+++ ufl/algorithms/comparison_checker.py
@@ -2,15 +2,126 @@
 """Algorithm to check for 'comparison' nodes
 in a form when the user is in 'complex mode'"""
 
-from ufl.corealg.multifunction import MultiFunction
+from ufl.corealg.multifunction import reuse_if_untouched
 from ufl.algorithms.map_integrands import map_integrand_dags
-from ufl.algebra import Real
+from ufl.classes import (Expr, GT, LT, GE, LE, MaxValue, MinValue, Sqrt, Power,
+                         Abs, Terminal, Indexed)
+from ufl.algebra import (Real, Imag)
 from ufl.constantvalue import RealValue, Zero
 from ufl.argument import Argument
 from ufl.geometry import GeometricQuantity
+from functools import singledispatch
 
 
-class CheckComparisons(MultiFunction):
+@singledispatch
+def check_comparisons(o, self, *ops):
+    """Single-dispatch function to error if comparisons are done with complex quantities
+    :arg o: UFL expression
+    :arg self: class that holds the nodetype dictionary
+
+    """
+    raise AssertionError("UFL node expected, not %s" % type(o))
+
+
+@check_comparisons.register(Expr)
+def check_comparisons_expr(o, self, *ops):
+    """Defaults expressions to complex unless they only
+        act on real quantities. Overridden for specific operators.
+
+        Rebuilds objects if necessary.
+        """
+
+    types = {self.nodetype[op] for op in ops}
+
+    if types:
+        t = "complex" if "complex" in types else "real"
+    else:
+        t = "complex"
+
+    o = reuse_if_untouched(o, *ops)
+    self.nodetype[o] = t
+    return o
+
+
+@check_comparisons.register(GT)
+@check_comparisons.register(LT)
+@check_comparisons.register(GE)
+@check_comparisons.register(LE)
+# Sign type should be here but isn't a class
+def check_comparisions_compare(o, self, *ops):
+    types = {self.nodetype[op] for op in ops}
+
+    if "complex" in types:
+        raise ComplexComparisonError("Ordering undefined for complex values.")
+    else:
+        o = o._ufl_expr_reconstruct_(*map(Real, ops))
+        self.nodetype[o] = "bool"
+        return o
+
+
+@check_comparisons.register(MaxValue)
+@check_comparisons.register(MinValue)
+def check_comparisons_max_value(o, self, *ops):
+    types = {self.nodetype[op] for op in ops}
+
+    if "complex" in types:
+        raise ComplexComparisonError("You can't compare complex numbers with max/min.")
+    else:
+        o = o._ufl_expr_reconstruct_(*map(Real, ops))
+        self.nodetype[o] = "bool"
+        return o
+
+
+@check_comparisons.register(Real)
+@check_comparisons.register(Imag)
+@check_comparisons.register(Abs)
+def check_comparisions_real(o, self, *ops):
+    o = reuse_if_untouched(o, *ops)
+    self.nodetype[o] = 'real'
+    return o
+
+
+@check_comparisons.register(Sqrt)
+def check_comparisions_sqrt(o, self, *ops):
+    o = reuse_if_untouched(o, *ops)
+    self.nodetype[o] = 'complex'
+    return o
+
+
+@check_comparisons.register(Power)
+def check_comparisions_power(o, self, base, exponent):
+    o = reuse_if_untouched(o, base, exponent)
+    try:
+        # Attempt to diagnose circumstances in which the result must be real.
+        exponent = float(exponent)
+        if self.nodetype[base] == 'real' and int(exponent) == exponent:
+            self.nodetype[o] = 'real'
+            return o
+    except TypeError:
+        pass
+
+    self.nodetype[o] = 'complex'
+    return o
+
+
+@check_comparisons.register(Terminal)
+def check_comparisions_terminal(term, self, *ops):
+    # default terminals to complex, except the ones we *know* are real
+    if isinstance(term, (RealValue, Zero, Argument, GeometricQuantity)):
+        self.nodetype[term] = 'real'
+    else:
+        self.nodetype[term] = 'complex'
+    return term
+
+
+@check_comparisons.register(Indexed)
+def check_comparisons_indexed(o, self, expr, multiindex):
+    o = reuse_if_untouched(o, expr, multiindex)
+    self.nodetype[o] = self.nodetype[expr]
+    return o
+
+
+class CheckComparisons(object):
     """Raises an error if comparisons are done with complex quantities.
 
     If quantities are real, adds the Real operator to the compared quantities.
@@ -23,8 +134,12 @@ class CheckComparisons(MultiFunction):
     """
 
     def __init__(self):
-        MultiFunction.__init__(self)
+        # MultiFunction.__init__(self)
         self.nodetype = {}
+        self.function = check_comparisons
+
+    def __call__(self, node, *args):
+        return self.function(node, self, *args)
 
     def expr(self, o, *ops):
         """Defaults expressions to complex unless they only
@@ -58,7 +173,11 @@ class CheckComparisons(MultiFunction):
     lt = compare
     ge = compare
     le = compare
-    sign = compare
+
+    def sign(self, o, *ops):
+        # Is it even possible to use this - sign is not a ufl class.
+        raise Warning("This is not implemented in the single dispatch version")
+        return self.compare(o, *ops)
 
     def max_value(self, o, *ops):
         types = {self.nodetype[op] for op in ops}
