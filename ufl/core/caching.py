51288bb9a747ef2c4619d82d360bb906434c405c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-04-22 13:55:20 +0100

    new file

---
+++ ufl/core/caching.py
@@ -0,0 +1,33 @@
+"""Caching.
+
+Custom caching function to be used with class methods.
+"""
+
+__cache = {}
+
+
+def cache(f):
+    """Decorator for caching the result of a function."""
+
+    def cached_f(self, *args, **kwargs):
+        global __cache
+        key = f"{args} {kwargs}"
+        if self not in __cache[f.__name__]:
+            __cache[f.__name__][self] = {}
+        if key not in __cache[f.__name__][self]:
+            __cache[f.__name__][self][key] = f(self, *args, **kwargs)
+        return __cache[f.__name__][self][key]
+
+    return cached_f
+
+
+def initialise_cache(function_name: str):
+    """Initialise a cacge for a given function."""
+    global __cache
+    __cache[function_name] = {}
+
+
+def empty_cache(function_name: str):
+    """Remove the cache for a given function."""
+    global __cache
+    del __cache[function_name]
