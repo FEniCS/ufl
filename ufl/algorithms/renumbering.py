270801edf791f47fe9a095c543761938bda10845
jorgensd <dokken92@gmail.com>
2024-09-19 18:29:08 +0000

    Fix zero index-renumbering for scalar zero

--- ufl/algorithms/renumbering.py
+++ ufl/algorithms/renumbering.py
@@ -48,6 +48,8 @@ class IndexRenumberingTransformer(VariableRenumberingTransformer):
         """Apply to zero."""
         fi = o.ufl_free_indices
         fid = o.ufl_index_dimensions
+        if fi == () and fid ==():
+            return o
         mapped_fi = tuple(self.index(Index(count=i)) for i in fi)
         paired_fid = [(mapped_fi[pos], fid[pos]) for pos, a in enumerate(fi)]
         new_fi, new_fid = zip(*tuple(sorted(paired_fid)))
