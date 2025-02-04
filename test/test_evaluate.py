65a8a030f22c6da14966c1921c56479c119b641b
jorgensd <dokken92@gmail.com>
2025-02-04 19:16:57 +0000

    Add test of knook fix

--- test/test_evaluate.py
+++ test/test_evaluate.py
@@ -2,7 +2,8 @@ __authors__ = "Martin Sandve Alnæs"
 __date__ = "2009-02-13 -- 2009-02-13"
 
 import math
-
+import numpy as np
+from ufl.constantvalue import ConstantValue
 from ufl import (
     Argument,
     Coefficient,
@@ -37,6 +38,22 @@ from ufl.constantvalue import as_ufl
 from ufl.finiteelement import FiniteElement
 from ufl.pullback import identity_pullback
 from ufl.sobolevspace import H1
+
+
+class CustomConstant(ConstantValue):
+    def __init__(self, value):
+        super().__init__()
+        self._value = value
+
+    @property
+    def ufl_shape(self):
+        return ()
+
+    def evaluate(self, x, mapping, component, index_values):
+        return self._value
+
+    def __repr__(self):
+        return f"CustomConstant({self._value})"
 
 
 def testScalars():
@@ -130,6 +147,18 @@ def testAlgebra():
     e = s((5, 7))
     v = 3 * (5.0 + 7.0) - 7 + 5.0 ** (7.0 / 2)
     assert e == v
+
+
+def testConstant():
+    _a = np.complex128(1 + 1j)
+    _b = np.complex128(-3 + 2j)
+    a = CustomConstant(_a)
+    b = CustomConstant(_b)
+    expr = a / b
+    e = expr(())
+
+    expected = complex(_a) / complex(_b)
+    assert e == expected
 
 
 def testIndexSum():
