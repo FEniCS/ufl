4bc7f0d208a4cbd2e45517c622e929b81a2a95b1
Pablo Brubeck <brubeck@protonmail.com>
2025-04-11 11:28:04 +0100

    GateauxDerivativeRuleset return zero of the correct shape (#371)
    * Hot fix for product rule of conditional
    
    * ruff
    
    * Fix derivative
    
    * Fix zero derivative
    
    * assertions in test
    
    * Remove Conditional._simplify_indexed
    
    * sort free indices
    
    * Fix exprequals
    
    * Restore early exit

--- ufl/exprequals.py
+++ ufl/exprequals.py
@@ -20,7 +20,7 @@ def expr_equals(self, other):
         return False
 
     # Large objects are costly to compare with themselves
-    if self is other or self.ufl_operands is other.ufl_operands:
+    if (self is other) or (self.ufl_operands is other.ufl_operands):
         return True
 
     # Modelled after pre_traversal to avoid recursion:
@@ -37,7 +37,7 @@ def expr_equals(self, other):
             # Delve into subtrees
             so = s.ufl_operands
             oo = o.ufl_operands
-            # Skip subtrees if operands are the same
+            # Skip subtree if operands are the same
             if so is oo:
                 continue
             if len(so) != len(oo):
@@ -48,7 +48,7 @@ def expr_equals(self, other):
                 if s._ufl_typecode_ != o._ufl_typecode_:
                     return False
                 # Skip subtree if objects are the same
-                if s is o or s.ufl_operands is o.ufl_operands:
+                if s is o:
                     continue
                 if (id(s), id(o)) in equal_pairs:
                     continue
