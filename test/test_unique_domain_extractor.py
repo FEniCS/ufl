8722709a43688bfa8406e47e04529ddace500136
Leo <leocollins511@gmail.com>
2025-07-03 15:02:31 +0100

    add form test

--- test/test_unique_domain_extractor.py
+++ test/test_unique_domain_extractor.py
@@ -7,10 +7,12 @@ from ufl import (
     Mesh,
     MeshSequence,
     SpatialCoordinate,
+    Measure,
     TrialFunction,
     split,
     triangle,
-    cos
+    cos,
+    inner
 )
 
 from ufl.domain import extract_unique_domain_dag
@@ -47,3 +49,28 @@ def test_extract_unique_domain():
     with pytest.raises(ValueError) as e_info:
         _ = extract_unique_domain_dag(u1 + u2)
         _ = extract_unique_domain_dag(u1 + u2 + x2 * cos(x2 * u1))
+
+
+def test_extract_unique_domain_form():
+    cell = triangle
+    elem0 = LagrangeElement(cell, 1)
+    elem1 = FiniteElement("Brezzi-Douglas-Marini", cell, 2, (2,), contravariant_piola, HDiv)
+    elem2 = FiniteElement("Discontinuous Lagrange", cell, 1, (), identity_pullback, L2)
+    elem = MixedElement([elem0, elem1, elem2])
+    mesh1 = Mesh(LagrangeElement(cell, 1, (2,)), ufl_id=100)
+    mesh2 = Mesh(LagrangeElement(cell, 1, (2,)), ufl_id=101)
+    mesh3 = Mesh(LagrangeElement(cell, 1, (2,)), ufl_id=102)
+    domain = MeshSequence([mesh1, mesh2, mesh3])
+    V = FunctionSpace(domain, elem)
+
+    u = TrialFunction(V)
+    u1, u2, u3 = split(u)
+
+    f = Coefficient(V)
+    f1, f2, f3 = split(f)
+
+    dx1 = Measure("dx", mesh1)
+
+    form1 = inner(u1, f1) * dx1
+
+    assert extract_unique_domain_dag(form1) == mesh1
