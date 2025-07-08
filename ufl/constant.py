7dffe548679474a55e2e39d82ccc5a50787a6403
Leo <leocollins511@gmail.com>
2025-07-08 16:32:09 +0100

    fix imports

--- ufl/constant.py
+++ ufl/constant.py
@@ -9,6 +9,7 @@
 from ufl.core.terminal import Terminal
 from ufl.core.ufl_type import ufl_type
 from ufl.utils.counted import Counted
+from ufl.domain import as_domain
 
 
 @ufl_type()
@@ -19,7 +20,6 @@ class Constant(Terminal, Counted):
 
     def __init__(self, domain, shape=(), count=None):
         """Initalise."""
-        from ufl.domain import as_domain
         Terminal.__init__(self)
         Counted.__init__(self, count, Constant)
 
