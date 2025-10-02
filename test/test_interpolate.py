0ef73219a4b27741dc2a9833eeece3510c93ede8
Leo Collins <collins.leo@proton.me>
2025-10-02 18:35:03 +0100

    remove dual arg numbers from expr_arg_numbers; add composition tests; add check for expression rank and size

--- test/test_interpolate.py
+++ test/test_interpolate.py
@@ -12,6 +12,7 @@ from ufl import (
     Argument,
     Coefficient,
     Cofunction,
+    Coargument,
     FunctionSpace,
     Mesh,
     TestFunction,
@@ -52,6 +53,11 @@ def V1(domain_2d):
 @pytest.fixture
 def V2(domain_2d):
     f1 = FiniteElement("CG", triangle, 2, (), identity_pullback, H1)
+    return FunctionSpace(domain_2d, f1)
+
+@pytest.fixture
+def V3(domain_2d):
+    f1 = FiniteElement("CG", triangle, 3, (), identity_pullback, H1)
     return FunctionSpace(domain_2d, f1)
 
 
@@ -244,3 +250,30 @@ def test_interpolate_argument_numbering(V1, V2):
         ValueError, match=r"Can only interpolate expressions with zero or one argument."
     ):
         Interpolate(u2, vstar0)
+
+
+def test_interpolate_composition(V1, V2, V3):
+    u1 = Coefficient(V1)
+    u2 = Interpolate(u1, V2)
+    u3 = Interpolate(u2, V3)
+ 
+    assert u3.ufl_function_space() == V3
+    assert u3.arguments()[0] == Argument(V3.dual(), 0)
+
+def test_interpolate_shape_rank(domain_2d):
+    f_scalar = FiniteElement("CG", triangle, 3, (), identity_pullback, H1)
+    f_vector2 = FiniteElement("CG", triangle, 3, (2,), identity_pullback, H1)
+    f_vector3 = FiniteElement("CG", triangle, 3, (3,), identity_pullback, H1)
+
+    V_scalar = FunctionSpace(domain_2d, f_scalar)
+    V_vector2 = FunctionSpace(domain_2d, f_vector2)
+    V_vector3 = FunctionSpace(domain_2d, f_vector3)
+
+    u_scalar = Coefficient(V_scalar)
+    u_vector2 = Coefficient(V_vector2)
+
+    with pytest.raises(ValueError, match=fr"Rank mismatch: Expression rank {len(u_scalar.ufl_shape)}, FunctionSpace rank {len(V_vector2.value_shape)}"):
+        Interpolate(u_scalar, V_vector2)
+
+    with pytest.raises(ValueError, match=fr"Shape mismatch: Expression shape \({u_vector2.ufl_shape}\), FunctionSpace shape \({V_vector3.value_shape}\)"):
+        Interpolate(u_vector2, V_vector3)
