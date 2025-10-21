0b2acbbdc342e04c11d06491107172426f3b72d6
Leo Collins <collins.leo@proton.me>
2025-10-21 10:46:45 +0100

    add `base_form_ops_as_expr` kwarg to extract_type
    added test + fix
    
    fixes

--- test/test_interpolate.py
+++ test/test_interpolate.py
@@ -34,6 +34,7 @@ from ufl.algorithms.analysis import (
 )
 from ufl.algorithms.expand_indices import expand_indices
 from ufl.core.interpolate import Interpolate
+from ufl.form import FormSum
 from ufl.pullback import identity_pullback
 from ufl.sobolevspace import H1
 
@@ -267,7 +268,9 @@ def test_interpolate_argument_numbering(V1, V2):
 def test_interpolate_composition(V1, V2, V3, V4, V5):
     u5 = Coefficient(V5)
     u4 = Interpolate(u5, V4)
+    assert u4.arguments() == (Argument(V4.dual(), 0),)
     u3 = Interpolate(u4, V3)
+    assert u3.arguments() == (Argument(V3.dual(), 0),)
     u2 = Interpolate(u3, V2)
     u1 = Interpolate(u2, V1)
 
@@ -283,9 +286,8 @@ def test_interpolate_composition(V1, V2, V3, V4, V5):
         Argument(V2.dual(), 0),
         Argument(V1.dual(), 0),
     }
-    assert set(u1.arguments()) == set(args)
 
-    # assert u1.arguments() == (Argument(V1.dual(), 0),)
+    assert u1.arguments() == (Argument(V1.dual(), 0),)
 
     # adjoint
     u1 = Cofunction(V1.dual())
@@ -310,7 +312,7 @@ def test_interpolate_composition(V1, V2, V3, V4, V5):
     assert u5.arguments() == (Argument(V5, 0),)
 
 
-def test_interpolate_sum(V1, V2, V3, V4):
+def test_interpolate_expr(V1, V2, V3, V4):
     u4 = Coefficient(V4)
     u31 = Interpolate(u4, V3)
     u32 = Interpolate(u4, V3)
@@ -318,9 +320,24 @@ def test_interpolate_sum(V1, V2, V3, V4):
     u2 = Interpolate(u3, V2)
     u1 = Interpolate(u2, V1)
 
+    assert extract_arguments(u3) == []
     assert u1.ufl_function_space() == V1
-    args = extract_arguments(u1)
-    assert set(args) == {Argument(V3.dual(), 0), Argument(V2.dual(), 0), Argument(V1.dual(), 0)}
+    assert set(extract_arguments(u1)) == {
+        Argument(V3.dual(), 0),
+        Argument(V2.dual(), 0),
+        Argument(V1.dual(), 0),
+    }
+    assert u1.arguments() == (Argument(V1.dual(), 0),)
+    assert u2.arguments() == (Argument(V2.dual(), 0),)
+
+    # product
+    u33 = u31 * u32
+    assert extract_arguments(u33) == []
+    assert extract_arguments(u33, base_form_ops_as_expr=False) == [Argument(V3.dual(), 0)]
+    u21 = Interpolate(u33, V2)
+    u11 = Interpolate(u21, V1)
+    assert u11.arguments() == (Argument(V1.dual(), 0),)
+    assert u21.arguments() == (Argument(V2.dual(), 0),)
 
     # adjoint
     u1 = Cofunction(V1.dual())
@@ -330,7 +347,99 @@ def test_interpolate_sum(V1, V2, V3, V4):
     u3 = Interpolate(Argument(V3, 0), u2)
     u4 = Interpolate(Argument(V4, 0), u3)
 
+    assert u1.arguments() == (Argument(V1, 0),)
+    assert extract_arguments(u2) == [Argument(V2, 0)]
     assert u4.ufl_function_space() == V4.dual()
-    args = extract_arguments(u4)
-    assert set(args) == {Argument(V2, 0), Argument(V3, 0), Argument(V4, 0)}
+    assert set(extract_arguments(u4)) == {Argument(V2, 0), Argument(V3, 0), Argument(V4, 0)}
     assert u4.arguments() == (Argument(V4, 0),)
+    assert u3.arguments() == (Argument(V3, 0),)
+
+
+def test_interpolate_cofunction(V1, V2):
+    V1_cofunc = Cofunction(V1.dual())
+    V2_test = TestFunction(V2)
+
+    Iu_adj = Interpolate(V2_test, V1_cofunc)  # Cofunction in V2^*
+    assert Iu_adj.arguments() == (Argument(V2, 0),)
+
+    # Test zero-form
+    V1_coeff = Coefficient(V1)
+    V2_coeff = Interpolate(V1_coeff, V2)
+    Iu_zeroform = Interpolate(V2_coeff, V1_cofunc)
+    assert Iu_zeroform.arguments() == ()
+
+    Iu2 = Interpolate(Iu_zeroform * V1_coeff, V2)
+    assert Iu2.arguments() == (Argument(V2.dual(), 0),)
+
+    Iu3 = V1_coeff * Iu_zeroform
+    assert extract_arguments(Iu3) == []
+
+
+def test_interpolate_form(V1, V2):
+    V1_coeff = Coefficient(V1)
+    V1_test = TestFunction(V1)
+    V1_trial = TrialFunction(V1)
+
+    Iu = Interpolate(V1_coeff, V2)
+    F = V1_test * Iu * dx
+
+    assert extract_arguments(F) == [V1_test]
+    assert extract_terminals_with_domain(F) == ([V1_test], [V1_coeff], [])
+
+    # Forward interpolation moves trial function from V1 to V2
+    V2_trial = TrialFunction(V2)
+    Iu = Interpolate(V2_trial, V1)  # V2 x V1^* -> R, equiv V2 -> V1
+    F = Iu * V1_test * dx
+    assert extract_arguments(F) == [V1_test, V2_trial]
+    assert F.arguments() == (V1_test, V2_trial)
+
+    F_act = V1_trial * V1_test * dx  # V1 x V1 -> R, equiv V1 -> V1^*
+    G = action(F_act, Iu)  # V2 -> V1 -> V1^*, equiv V2 x V1 -> R
+    assert extract_arguments(G) == [V1_test, V2_trial]
+    assert G.arguments() == (V1_test, V2_trial)
+
+    # Adjoint interpolation moves test function from V1 to V2
+    V2_test = TestFunction(V2)
+    Iu_adj = Interpolate(V2_test, V1)  # V1^* x V2 -> R, equiv V1^* -> V2^*
+    F = V1_trial * Iu_adj * dx
+    assert extract_arguments(F) == [V2_test, V1_trial]
+    assert F.arguments() == (V2_test, V1_trial)
+    G2 = action(Iu_adj, F_act)  # V1 -> V1^* -> V2^*, equiv V1 x V2 -> R
+    assert extract_arguments(G2) == [V2_test, V1_trial]
+    assert G2.arguments() == (V2_test, V1_trial)
+
+    # alternatively using adjoint(Iu)
+    Iu_adj = adjoint(Iu)
+    G2 = action(Iu_adj, F_act)
+    assert extract_arguments(G2) == [V2_test, V1_trial]
+    assert G2.arguments() == (V2_test, V1_trial)
+
+    G3 = Interpolate(V2_test, F_act)
+    assert set(extract_arguments(G3)) == {V1_test, V2_test, V1_trial}
+    assert G3.arguments() == (V2_test, V1_trial)
+
+
+def test_interpolate_adjoint(V1, V2):
+    V1_trial = TrialFunction(V1)
+    Iu_forward = Interpolate(V1_trial, V2)  # V1 x V2^* -> R, equiv V1 -> V2
+    Iu_adjoint = adjoint(Iu_forward)  # V2^* x V1 -> R, equiv V2^* -> V1^*
+
+    assert Iu_forward.arguments() == (Argument(V2.dual(), 0), V1_trial)
+    assert Iu_adjoint.arguments() == (Argument(V1, 0), Argument(V2.dual(), 1))
+
+    Iu_adjoint2 = Interpolate(Argument(V1, 0), Argument(V2.dual(), 1))
+    assert Iu_adjoint2.arguments() == Iu_adjoint.arguments()
+
+    Iu = adjoint(Iu_adjoint)
+    assert Iu.arguments() == Iu_forward.arguments()
+
+
+def test_interpolate_formsum(V1, V2):
+    V1_cofunc = Cofunction(V1.dual())
+    V1_cofunc2 = Cofunction(V1.dual())
+    V1_sum = V1_cofunc + V1_cofunc2
+    assert isinstance(V1_sum, FormSum)
+    assert V1_sum.arguments() == (Argument(V1, 0),)
+
+    V2_cofunc = Interpolate(Argument(V2, 0), V1_sum)  # Cofunction in V2^*
+    assert V2_cofunc.arguments() == (Argument(V2, 0),)
