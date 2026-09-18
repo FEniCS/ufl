930217341236f9dc007c539c83f3a2b6f196a51e
Pablo Brubeck <brubeck@protonmail.com>
2026-09-18 15:15:00 +0100

    Clarify Measure subdomain_id documentation

--- ufl/measure.py
+++ ufl/measure.py
@@ -127,7 +127,7 @@ class Measure:
                 the integral_type on the primal domain in a multi-domain problem.
             domain: an AbstractDomain object (most often a Mesh);
                 the primal domain in a multi-domain problem.
-            subdomain_id: a string, a single subdomain id int, or a tuple of ints and strings
+            subdomain_id: a single subdomain id int or string, or a tuple of ints and strings
             metadata: dict, with additional compiler-specific parameters
                 affecting how code is generated, including parameters
                 for optimization or debugging of generated code
