23af936c1b8864420aee79322b3d53f5823146af
Jørgen Schartum Dokken <dokken92@gmail.com>
2026-09-21 12:38:18 +0200

    Linting

--- ufl/algorithms/check_arities.py
+++ ufl/algorithms/check_arities.py
@@ -265,8 +265,8 @@ def check_integrand_arity(expr, arguments, complex_mode=False):
     """Check the arity of an integrand.
 
     Arity extraction records each argument as an ``(argument, is_conjugated)`` pair. This enables
-    complex-mode checks to validate the required state: test functions are always conjugated, whereas
-    trial functions remain unconjugated.
+    complex-mode checks to validate the required state: test functions are always conjugated,
+    whereas trial functions remain unconjugated.
     """
     arguments = tuple(sorted(set(arguments), key=lambda x: (x.number(), x.part())))
     rules = ArityChecker(arguments)
